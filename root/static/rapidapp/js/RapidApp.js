ec4e50469815ab519d1d38746c897a8f4d4d4995
Michael Conrad <mconrad@intellitree.com>
2010-11-12 02:19:36 -0500

    Added history support, and prevention of back button killing of the app.

--- root/static/rapidapp/js/RapidApp.js
+++ root/static/rapidapp/js/RapidApp.js
@@ -2701,6 +2701,80 @@ Ext.override(Ext.ux.ComponentDataView, {
 	 }
 });
 
+Ext.ns('Ext.ux.RapidApp.History');
+Ext.ux.RapidApp.History.recordHistEvent=
+	function(id, oldval, newval) {
+		var nextIdx= Ext.History.eventIdx < 9? Ext.History.eventIdx+1 : 0;
+		var eventText= ''+nextIdx+':'+id+':'+oldval+':'+newval;
+		if (oldval && eventText != Ext.History.ignoreEvent) {
+			//console.log('recordHistEvent '+eventText);
+			Ext.History.newestEvent= eventText;
+			Ext.History.eventIdx= nextIdx;
+			Ext.History.add(eventText);
+		}
+		else {
+			//console.log('recordHistEvent '+eventText+' (ignored)');
+			if (!Ext.History.newestEvent) {
+				Ext.History.newestEvent= ''+Ext.History.eventIdx+':::';
+				Ext.History.add(Ext.History.newestEvent);
+			}
+		}
+	};
+
+Ext.ux.RapidApp.History.eventIdx= 0;
+Ext.ux.RapidApp.History.ignoreEvent= '';
+Ext.ux.RapidApp.History.handleHistChange=
+	function(token) {
+		Ext.History.ignoreEvent= '';
+		//console.log('onChange: '+Ext.History.newestEvent+' => '+token);
+		if (Ext.History.newestEvent != token) {
+			var parts= token? token.split(':') : [ ];
+			var newIdx= token? parseInt(parts[0]) : Ext.History.eventIdx;
+			var nextIdx= newIdx < 9? newIdx+1 : 0;
+			var diff= token? newIdx - Ext.History.eventIdx : -1;
+			var prevEvent= Ext.History.newestEvent;
+			Ext.History.newestEvent= token;
+			Ext.History.eventIdx= newIdx;
+			
+			if (diff <= -5 || (diff > 0 && diff < 5)) { // navigating forward
+				if (parts[1] && parts[2] && parts[3]) {
+					var tabPanel= Ext.getCmp(parts[1]);
+					Ext.History.ignoreEvent= ''+nextIdx+':'+parts[1]+':'+parts[2]+':'+parts[3];
+					tabPanel.setActiveTab(parts[3]);
+				}
+			}
+			else if (prevEvent) { // navigating backward
+				parts= prevEvent.split(':');
+				if (parts[1] && parts[2] && parts[3]) {
+					var tabPanel= Ext.getCmp(parts[1]);
+					Ext.History.ignoreEvent= ''+nextIdx+':'+parts[1]+':'+parts[3]+':'+parts[2];
+					tabPanel.setActiveTab(parts[2]);
+				}
+			}
+		}
+		if (!token) {
+			Ext.History.newestEvent= ''+Ext.History.eventIdx+':::';
+			Ext.History.add(Ext.History.newestEvent);
+		}
+		//console.log('  newest='+Ext.History.newestEvent+' ign='+Ext.History.ignoreEvent+' idx='+Ext.History.eventIdx);
+	};
+
+Ext.onReady(function() {
+	Ext.History.init();
+	Ext.History.on('change', function(token) { Ext.ux.RapidApp.History.handleHistChange(token); });
+});
+
+Ext.override(Ext.TabPanel,{
+	initComponent_orig: Ext.TabPanel.prototype.initComponent,
+	initComponent: function() {
+		this.initComponent_orig.apply(this,arguments);
+		
+		this.on('beforetabchange', function(tabPanel, newTab, currentTab){
+			  Ext.ux.RapidApp.History.recordHistEvent(tabPanel.id, currentTab? currentTab.id : "", newTab? newTab.id : "");
+			  return true;
+		 });
+	}
+});
 
 Ext.ns('Ext.ux');
 Ext.ux.TplTabPanel = Ext.extend(Ext.TabPanel, {
@@ -2770,5 +2844,4 @@ Ext.ux.FieldHelp = Ext.extend(Object, (function(){
 	 };
 })());
 Ext.preg('fieldhelp',Ext.ux.FieldHelp);
-
 
