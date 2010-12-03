6fb3340a128e39265909da79994e754dcfc4911e
Henry Van Styn <vanstyn@intellitree.com>
2010-12-03 08:24:52 -0500

    Started creating Ext.ux.RapidApp.AppStoreForm2.FormPanel

---
+++ root/static/rapidapp/js/RapidApp/AppStoreForm2.js
@@ -0,0 +1,7 @@
+Ext.ns('Ext.ux.RapidApp.AppStoreForm2');
+
+Ext.ux.RapidApp.AppStoreForm2.FormPanel = Ext.extend(Ext.form.FormPanel,{
+
+
+});
+Ext.reg('appstoreform2', Ext.ux.RapidApp.AppStoreForm2.FormPanel);
