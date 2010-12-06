24f53b056aa3dfa46255bd734e1effc15ec11cae
Michael Conrad <mconrad@intellitree.com>
2010-12-06 12:42:14 -0500

    Added new utility methods to ScopedGlobals, and added comments.

--- lib/RapidApp/ScopedGlobals.pm
+++ lib/RapidApp/ScopedGlobals.pm
@@ -7,6 +7,22 @@ use warnings;
 
 RapidApp::ScopedGlobals
 
+=head1 SYNOPSIS
+
+  use RapidApp::ScopedGlobals;
+  sub foo_0 {
+    RapidApp::ScopedGlobals->applyForSub({ x => 1 }, \&foo_1 );
+  }
+  sub foo_1 {
+    RapidApp::ScopedGlobals->applyForSub({ x => 2 }, \&foo_2 );
+    print RapidApp::ScopedGlobals->x;
+  }
+  sub foo_2 {
+    print RapidApp::ScopedGlobals->x, ",";
+  }
+  
+  # calling foo_0 prints "2,1"
+
 =head1 DESCRIPTION
 
 This ScopedGlobals package implements a type of "constant environment" global.  No code may
@@ -20,10 +36,19 @@ which could be seen by a parent).  Instead, they act much more like invisible pa
 method, which can help when working with frameworks where it is impossible or just inconvenient
 to pass those parameters to an inner module or method.
 
+=head1 METHODS
+
 =cut
 
 our $_vals= {};
 
+=head2 applyForSub( \%,  \CODE )
+
+Calls the given coderef with the given alterations of the "global" variables.  When the coderef
+returns, the global values will revert to what they were before the call.  See perl's "local"
+keyword for implementation details.
+
+=cut
 sub applyForSub {
 	my ($class, $varChangeHash, $sub)= @_;
 	$class eq __PACKAGE__ or die "applyForSub is a package method";
@@ -34,6 +59,34 @@ sub applyForSub {
 	return $sub->();
 }
 
+=head2 varExists( $varName )
+
+Returns true of the named global has been set by a parent function of the current call stack.
+
+=cut
+sub varExists {
+	my ($class, $varName)= @_;
+	return exists $_vals->{$varName};
+}
+
+=head2 get( $varName )
+
+Returns the value of the named scoped-global, or undef if it has not been set.  Note that
+a scoped-global could have been set to undef.  Use "varExists" to determine the difference.
+
+=cut
+sub get {
+	my ($class, $varName)= @_;
+	return $_vals->{$varName};
+}
+
+=head AUTOLOAD
+
+RapidApp::ScopedGlobals defines an auto-loader that allows you to use any named global as if it
+were a method.  If the global has not been defined in this call stack, an exception is thrown.
+This allows better debugging of typo'd var names, but if you don't want an exception, use "get".
+
+=cut
 our $AUTOLOAD; # built-in package global
 sub AUTOLOAD {
 	my $vName= substr($AUTOLOAD, length(__PACKAGE__)+2);
