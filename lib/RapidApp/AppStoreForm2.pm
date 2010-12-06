cc85f5cbd4bb941b88d0edc2b2bf05ac116f1d70
Henry Van Styn <vanstyn@intellitree.com>
2010-12-06 18:13:12 -0500

    Started moving code into new Role: RapidApp::Role::DataStore2

--- lib/RapidApp/AppStoreForm2.pm
+++ lib/RapidApp/AppStoreForm2.pm
@@ -1,66 +1,20 @@
 package RapidApp::AppStoreForm2;
 use Moose;
 extends 'RapidApp::AppCmp';
+with 'RapidApp::Role::DataStore2';
 
 use strict;
 
 use RapidApp::Include qw(sugar perlutil);
 
-use RapidApp::DataStore2;
 
 has 'reload_on_save' 	=> ( is => 'ro', default => 0 );
-has 'record_pk'			=> ( is => 'ro', default => 'id' );
-has 'DataStore_class'	=> ( is => 'ro', default => 'RapidApp::DataStore2' );
-
 
 sub BUILD {
 	my $self = shift;
 	
-	my $store_params = { record_pk => $self->record_pk };
-	
-	if ($self->can('create_records')) {
-		$self->apply_flags( can_create => 1 ) unless ($self->flag_defined('can_create'));
-		$store_params->{create_handler}	= RapidApp::Handler->new( scope => $self, method => 'create_records' ) if ($self->has_flag('can_create'));
-	}
-	
-	if ($self->can('read_records')) {
-		$self->apply_flags( can_read => 1 ) unless ($self->flag_defined('can_read'));
-		$store_params->{read_handler}	= RapidApp::Handler->new( scope => $self, method => 'read_records' ) if ($self->has_flag('can_read'));
-	}
-	
-	if ($self->can('update_records')) {
-		$self->apply_flags( can_update => 1 ) unless ($self->flag_defined('can_update'));
-		$store_params->{update_handler}	= RapidApp::Handler->new( scope => $self, method => 'update_records' ) if ($self->has_flag('can_update'));
-	}
-	
-#	if ($self->can('destroy_records')) {
-#		$self->apply_flags( can_destroy => 1 ) unless ($self->flag_defined('can_destroy'));
-#		$store_params->{destroy_handler}	= RapidApp::Handler->new( scope => $self, method => 'destroy_records' ) if ($self->has_flag('can_destroy'));
-#	}
-	
-	$self->apply_modules( store => {
-		class		=> $self->DataStore_class,
-		params	=> $store_params
-	});
-	
-	#init the store with all of our flags:
-	$self->Module('store',1)->apply_flags($self->all_flags);
-	
 	$self->Module('store',1)->add_listener( load => RapidApp::JSONFunc->new( raw => 1, func => 'Ext.ux.RapidApp.AppStoreForm2.store_load_handler' ));	
 	$self->Module('store',1)->add_listener( write => RapidApp::JSONFunc->new( raw => 1, func =>  'function(store) { store.load(); }' )) if ($self->reload_on_save); 
-
-	
-#	$self->Module('store',1)->apply_listeners(
-#
-#		load => RapidApp::JSONFunc->new( raw => 1, func => 'Ext.ux.RapidApp.AppStoreForm2.store_load_handler' ),
-#
-#		write => RapidApp::JSONFunc->new( raw => 1, func => 
-#			'function(store, action, result, res, rs) { ' .
-#				'console.log("write event");' . 
-#				($self->reload_on_save ? 'store.load();' : '') .
-#			'}' 
-#		)
-#	);
 
 
 	$self->apply_extconfig(
@@ -71,11 +25,6 @@ sub BUILD {
 	$self->add_listener( clientvalidation	=> RapidApp::JSONFunc->new( raw => 1, func => 'Ext.ux.RapidApp.AppStoreForm2.clientvalidation_handler' ) );
 	$self->add_listener( afterrender			=> RapidApp::JSONFunc->new( raw => 1, func => 'Ext.ux.RapidApp.AppStoreForm2.afterrender_handler' ) );
 
-#	$self->apply_listeners(
-#		clientvalidation	=> RapidApp::JSONFunc->new( raw => 1, func => 'Ext.ux.RapidApp.AppStoreForm2.clientvalidation_handler' ),
-#		afterrender			=> RapidApp::JSONFunc->new( raw => 1, func => 'Ext.ux.RapidApp.AppStoreForm2.afterrender_handler' )
-#	);
-	
 	$self->add_ONREQUEST_calls('init_onrequest');
 }
 
---
+++ lib/RapidApp/Role/DataStore2.pm
@@ -0,0 +1,61 @@
+package RapidApp::Role::DataStore2;
+use Moose::Role;
+
+use strict;
+
+use RapidApp::Include qw(sugar perlutil);
+
+use RapidApp::DataStore2;
+
+has 'record_pk'			=> ( is => 'ro', default => 'id' );
+has 'DataStore_class'	=> ( is => 'ro', default => 'RapidApp::DataStore2' );
+
+
+sub BUILD {}
+before 'BUILD' => sub {
+	my $self = shift;
+
+	my $store_params = { record_pk => $self->record_pk };
+	
+	if ($self->can('create_records')) {
+		$self->apply_flags( can_create => 1 ) unless ($self->flag_defined('can_create'));
+		$store_params->{create_handler}	= RapidApp::Handler->new( scope => $self, method => 'create_records' ) if ($self->has_flag('can_create'));
+	}
+	
+	if ($self->can('read_records')) {
+		$self->apply_flags( can_read => 1 ) unless ($self->flag_defined('can_read'));
+		$store_params->{read_handler}	= RapidApp::Handler->new( scope => $self, method => 'read_records' ) if ($self->has_flag('can_read'));
+	}
+	
+	if ($self->can('update_records')) {
+		$self->apply_flags( can_update => 1 ) unless ($self->flag_defined('can_update'));
+		$store_params->{update_handler}	= RapidApp::Handler->new( scope => $self, method => 'update_records' ) if ($self->has_flag('can_update'));
+	}
+	
+	if ($self->can('destroy_records')) {
+		$self->apply_flags( can_destroy => 1 ) unless ($self->flag_defined('can_destroy'));
+		$store_params->{destroy_handler}	= RapidApp::Handler->new( scope => $self, method => 'destroy_records' ) if ($self->has_flag('can_destroy'));
+	}
+	
+	$self->apply_modules( store => {
+		class		=> $self->DataStore_class,
+		params	=> $store_params
+	});
+	
+	#init the store with all of our flags:
+	$self->Module('store',1)->apply_flags($self->all_flags);
+	
+
+
+};
+
+
+
+
+
+#### --------------------- ####
+
+
+no Moose;
+#__PACKAGE__->meta->make_immutable;
+1;
