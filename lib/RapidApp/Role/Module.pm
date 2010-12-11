d642ad437f51cf8fc2344ee8db72324eb8411af6
Henry Van Styn <vanstyn@intellitree.com>
2010-12-10 19:35:16 -0500

    Cleaned up now unused code and removed debug messages

--- lib/RapidApp/Role/Module.pm
+++ lib/RapidApp/Role/Module.pm
@@ -106,18 +106,11 @@ has '_lastRequestApplied' => ( is => 'rw', default => 0 );
 sub ONREQUEST {
 	my $self = shift;
 	
-	$self->_lastRequestApplied($self->c->stash->{rapidapp_request_id}) if (defined $self->c);
-	
-	my $id = '(no req)';
-	$id = $self->c->stash->{rapidapp_request_id}  if (defined $self->c);
-	$self->app->log->debug(BOLD . ' --------------> ONREQUEST: ' . ref($self) . ': ' . $id . CLEAR);
+	$self->_lastRequestApplied($self->c->stash->{rapidapp_request_id});
 	
 	$self->call_rapidapp_handlers($self->all_ONREQUEST_calls);
 	
-	#foreach my $attr ($self->meta->get_all_attributes) {
-	
 	$self->new_clear_per_req_attrs;
-	
 	
 	$self->ONREQUEST_called(1);
 	return $self;
@@ -129,22 +122,19 @@ sub new_clear_per_req_attrs {
 	#$self->ONREQUEST_called(0);
 	
 	foreach my $attr (@{$self->cached_per_req_attr_list}) {
-		#if ($attr->does('RapidApp::Role::PerRequestBuildDefReset')) {
-			# Reset to default:
-			if(defined $self->per_request_attr_build_defaults->{$attr->name}) {
-				my $val = $self->per_request_attr_build_defaults->{$attr->name};
-				$val = clone($val) if (ref($val));
-				$attr->set_value($self,$val);
-			}
-			# Initialize default:
-			else {
-				my $val = $attr->get_value($self);
-				$val = clone($val) if (ref($val));
-				$self->per_request_attr_build_defaults->{$attr->name} = $val;
-			}
-		#}
+		# Reset to default:
+		if(defined $self->per_request_attr_build_defaults->{$attr->name}) {
+			my $val = $self->per_request_attr_build_defaults->{$attr->name};
+			$val = clone($val) if (ref($val));
+			$attr->set_value($self,$val);
+		}
+		# Initialize default:
+		else {
+			my $val = $attr->get_value($self);
+			$val = clone($val) if (ref($val));
+			$self->per_request_attr_build_defaults->{$attr->name} = $val;
+		}
 	}
-	
 }
 
 
@@ -153,11 +143,6 @@ sub new_clear_per_req_attrs {
 sub THIS_MODULE {
 	my $self = shift;
 	return $self unless (defined $self->c);
-	
-#	unless ($self->_lastRequestApplied) {
-#		return $self->ONREQUEST unless ($self->ONREQUEST_called);
-#		return $self;
-#	}
 	
 	return $self->ONREQUEST if (defined $self->c && $self->c->stash->{rapidapp_request_id} != $self->_lastRequestApplied);
 	return $self;
@@ -242,45 +227,6 @@ sub parent_by_name {
 	return $self->parent_module->parent_by_name($name);
 }
 
-sub recursive_clear_per_request_vars {
-	my $self= shift;
-	
-	# clear our own
-	$self->clear_per_request_vars;
-	
-	# now clean up all sub-modules
-	foreach my $subobj (values %{$self->modules_obj}) {
-		$subobj->recursive_clear_per_request_vars;
-	}
-}
-
-sub clear_per_request_vars {
-	my $self= shift;
-	
-	#my $listmsg= $self->c->log->is_debug? ($self->module_name? $self->module_name : '[root]').': Clearing' : undef;
-	
-	# if the no_persist property is true, clear ALL the lazy parameters
-	# note that no_persist is not quite the same as PerRequestVar, because PerRequestVar can also reset non-lazy attributes
-	if ($self->can('no_persist') && $self->no_persist) {
-		for my $attr ($self->meta->get_all_attributes) {
-			if ($attr->is_lazy or $attr->has_clearer) {
-				$attr->clear_value($self);
-				#defined $listmsg and $listmsg.= ' '.$attr->name;
-			}
-		}
-	}
-	
-	# clear all attributes which have the role 'PerRequestVar'
-	foreach my $attr (grep { Moose::Util::does_role($_, 'RapidApp::Role::PerRequestVar') } $self->meta->get_all_attributes) {
-		#defined $listmsg and $listmsg.= ' '.$attr->name;
-		$attr->clear_value($self);
-		# reset the default, if it isn't lazy
-		if (!$attr->is_lazy && $attr->has_default) {
-			$attr->set_initial_value($self, $attr->default($self));
-		}
-	}
-	#defined $listmsg and $self->c->log->debug($listmsg);
-}
 
 
 sub applyIf_module_options {
