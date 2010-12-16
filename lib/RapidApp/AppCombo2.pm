b09645b1f2951f8120b07ff33871d5a4d0ae07cb
Henry Van Styn <vanstyn@intellitree.com>
2010-12-15 20:01:20 -0500

    Added AppCombo2 - replacement for AppCombo
    AppCombo2 is based on AppCmp and DataStore2

---
+++ lib/RapidApp/AppCombo2.pm
@@ -0,0 +1,39 @@
+package RapidApp::AppCombo2;
+use Moose;
+extends 'RapidApp::AppCmp';
+with 'RapidApp::Role::DataStore2';
+
+use strict;
+
+use RapidApp::Include qw(sugar perlutil);
+
+
+has 'name' 					=> ( is => 'ro', required => 1, isa => 'Str' );
+has 'displayField' 		=> ( is => 'ro', required => 1, isa => 'Str' );
+has 'valueField' 			=> ( is => 'ro', required => 1, isa => 'Str' );
+has 'fieldLabel' 			=> ( is => 'ro', lazy => 1, default => sub { (shift)->name } );
+
+sub BUILD {
+	my $self = shift;
+	
+	$self->apply_extconfig(
+		xtype				=> 'combo',
+		typeAhead		=> \0,
+		mode				=> 'remote',
+		triggerAction	=> 'all',
+		selectOnFocus	=> \1,
+		editable			=> \0,
+		allowBlank 		=> \0,
+		width 			=> 337,
+		name 				=> $self->name,
+		fieldLabel 		=> $self->fieldLabel,
+		displayField 	=> $self->displayField,
+		valueField 		=> $self->valueField,
+	);
+}
+
+
+
+no Moose;
+#__PACKAGE__->meta->make_immutable;
+1;
