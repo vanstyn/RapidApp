014e5a00ea267e433b418ca60775e391ac97a13b
Henry Van Styn <vanstyn@intellitree.com>
2011-01-26 00:26:54 -0500

    Refactored BaseConditions with native traits for base_joins

--- lib/RapidApp/DBIC/ResultSet/BaseConditions.pm
+++ lib/RapidApp/DBIC/ResultSet/BaseConditions.pm
@@ -5,13 +5,53 @@ use namespace::autoclean;
 extends 'DBIx::Class::ResultSet';
 
 use RapidApp::Include qw(sugar perlutil);
+use Clone;
 
 sub c { RapidApp::ScopedGlobals->get('catalystInstance'); }
 
 has 'base_search_conditions' => ( is => 'ro', default => undef );
-has 'base_search_attrs' => ( is => 'ro', isa => 'Maybe[HashRef]', default => undef );
+
+has 'base_search_attrs' => ( 
+	is => 'ro', 
+	traits => [ 'Hash' ],
+	isa => 'HashRef', 
+	default => sub {{}},
+	handles => {
+		apply_base_search_attrs	=> 'set',
+	}
+);
+
+has 'base_search_joins' => ( 
+	is => 'ro', 
+	traits => [ 'Array' ],
+	isa => 'ArrayRef', 
+	default => sub {[]},
+	handles => {
+		add_base_search_joins	=> 'push',
+		all_base_search_joins	=> 'elements'
+	}
+);
+after 'add_base_search_joins' => sub {
+	my $self = shift;
+	$self->apply_base_search_attrs( join => [ $self->all_base_search_joins ] );
+
+};
 
 has 'override_search_rs' => ( is => 'ro', isa => 'Bool', default => 1 );
+
+sub BUILD {
+	my $self = shift;
+	
+	if ($self->base_search_attrs->{join}) {
+		if (ref($self->base_search_attrs) eq 'ARRAY') {
+			$self->add_base_search_joins(@{$self->base_search_attrs->{join}});
+		}
+		else {
+			$self->add_base_search_joins($self->base_search_attrs->{join});
+		}
+	}
+}
+
 
 sub search_rs {
 	my ($self, @args) = @_;
