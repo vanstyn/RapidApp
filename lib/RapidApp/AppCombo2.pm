d98bb4252746df0165fe50390b723275f9a8c623
Michael Conrad <mconrad@intellitree.com>
2011-01-26 00:43:36 -0500

    Added Web 1.0 renderers for AppGrid2 and AppCombo2

--- lib/RapidApp/AppCombo2.pm
+++ lib/RapidApp/AppCombo2.pm
@@ -32,19 +32,72 @@ sub BUILD {
 }
 
 sub web1_render {
-	my ($self, $renderCxt, $cfg)= @_;
+	my ($self, $renderCxt, $extCfg)= @_;
 	
-	$renderCxt->write('<div class="xt-appcombo2">');
-	my $value= $cfg->{value};
-	if (defined $value) {
-		for my $v (split(',',$value)) {
-			$renderCxt->write('<span class="val">'.$renderCxt->escHtml($v).'</span> ');
+	# simulate a get request to the grid's store
+	my $storeFetchParams= $extCfg->{store}{parm}{baseParams};
+	my $origParams= $self->c->req->params;
+	my $data;
+	try {
+		$self->c->req->params($storeFetchParams);
+		$data= $self->Module('store')->read();
+		$self->c->req->params($origParams);
+	}
+	catch {
+		$self->c->req->params($origParams);
+		die $_;
+	};
+	
+	# now we need to find the row that corresponds to the value
+	my $value= $extCfg->{value};
+	my $valueField= $extCfg->{valueField};
+	my $selectedRow;
+	
+	$self->c->log->debug((ref $self)." looking for $valueField=$value.' in ".Data::Dumper::Dumper($data->{rows}));
+	
+	if (ref $data->{rows} eq 'ARRAY' && scalar(@{$data->{rows}}) ) {
+		for my $row (@{$data->{rows}}) {
+			if ($value eq $row->{$valueField}) {
+				$selectedRow= $row;
+				last;
+			}
 		}
 	}
-	else {
-		$renderCxt->write('<span class="val"> </span>');
+	
+	$renderCxt->write('<div class="xt-appcombo2">');
+	if ($selectedRow) {
+		$self->web1_render_list_item($renderCxt, $selectedRow, $extCfg->{tpl});
 	}
-	$renderCxt->write('</div>');
+	else {
+		$renderCxt->write('<span class="val">&nbsp;</span>');
+	}
+	$renderCxt->write("</div>\n");
+}
+
+=pod
+
+This is a cheesy hack attempt at processing an xtemplate.
+If we come up with a better engine for that, replace this code with a call to it.
+In the meantime, you can override this method to do custom rendering in your module.
+
+=cut
+sub web1_render_list_item {
+	my ($self, $renderCxt, $row, $template)= @_;
+	my $text= $template;
+	$template =~ s|</?tpl[^>]+>||g;
+	my @parts= split /[{}]/, $template;
+	for (my $i=1; $i <= $#parts; $i+= 2) {
+		if (substr($parts[$i], 0, 1) eq '[') {
+			$self->c->log->warn("You need to write a custom 'web1_render_list_item' for ".(ref $self));
+			$parts[$i]= '[unrenderable content]';
+		}
+		else {
+			$parts[$i]= $renderCxt->escHtml($row->{$parts[$i]});
+		}
+	}
+	my $html= join '', @parts;
+	$self->c->log->debug("Before: $template\nAfter: $html");
+	$renderCxt->write($html);
 }
 
 no Moose;
