74b09a1a9a21040dce19c46bdf5f434a45da9407
Henry Van Styn <vanstyn@intellitree.com>
2011-03-22 10:26:23 -0400

    AppDV: Create record support

--- root/static/rapidapp/js/RapidApp/AppDV.js
+++ root/static/rapidapp/js/RapidApp/AppDV.js
@@ -2,8 +2,8 @@ Ext.ns('Ext.ux.RapidApp.AppDV');
 
 
 Ext.ux.RapidApp.AppDV.DataView = Ext.extend(Ext.DataView, {
-	 //defaultType: 'textfield',
-	 initComponent : function(){
+	//defaultType: 'textfield',
+	initComponent : function(){
 			Ext.each(this.items,function(item) {
 				item.ownerCt = this;
 			},this);
@@ -11,80 +11,98 @@ Ext.ux.RapidApp.AppDV.DataView = Ext.extend(Ext.DataView, {
 			this.components = [];
 			
 			this.on('click',this.click_controller,this);
-	 },
-	 
-	 refresh : function(){
-		  Ext.destroy(this.components);
-		  this.components = [];
-		  Ext.ux.RapidApp.AppDV.DataView.superclass.refresh.call(this);
-		  this.renderItems(0, this.store.getCount() - 1);
-	 },
-	 onUpdate : function(ds, record){
-		  var index = ds.indexOf(record);
-		  if(index > -1){
+	},
+	
+	refresh : function(){
+		Ext.destroy(this.components);
+		this.components = [];
+		Ext.ux.RapidApp.AppDV.DataView.superclass.refresh.call(this);
+		this.renderItems(0, this.store.getCount() - 1);
+	},
+	onUpdate : function(ds, record){
+		var index = ds.indexOf(record);
+		if(index > -1){
 				this.destroyItems(index);
-		  }
-		  Ext.ux.RapidApp.AppDV.DataView.superclass.onUpdate.apply(this, arguments);
-		  if(index > -1){
+		}
+		Ext.ux.RapidApp.AppDV.DataView.superclass.onUpdate.apply(this, arguments);
+		if(index > -1){
 				this.renderItems(index, index);
-		  }
-	 },
-	 onAdd : function(ds, records, index){
-		  var count = this.all.getCount();
-		  Ext.ux.RapidApp.AppDV.DataView.superclass.onAdd.apply(this, arguments);
-		  if(count !== 0){
+		}
+	},
+	onAdd : function(ds, records, index){
+		var count = this.all.getCount();
+		Ext.ux.RapidApp.AppDV.DataView.superclass.onAdd.apply(this, arguments);
+		if(count !== 0){
 				this.renderItems(index, index + records.length - 1);
-		  }
-	 },
-	 
-	 onRemove : function(ds, record, index){
-		  this.destroyItems(index);
-		  Ext.ux.RapidApp.AppDV.DataView.superclass.onRemove.apply(this, arguments);
-	 },
-	 onDestroy : function(){
-		  Ext.ux.RapidApp.AppDV.DataView.superclass.onDestroy.call(this);
-		  Ext.destroy(this.components);
-		  this.components = [];
-	 },
-	 renderItems : function(startIndex, endIndex){
-		  var ns = this.all.elements;
-		  var args = [startIndex, 0];
-		  for(var i = startIndex; i <= endIndex; i++){
+		}
+
+	},
+	
+	onRemove : function(ds, record, index){
+		this.destroyItems(index);
+		Ext.ux.RapidApp.AppDV.DataView.superclass.onRemove.apply(this, arguments);
+	},
+	onDestroy : function(){
+		Ext.ux.RapidApp.AppDV.DataView.superclass.onDestroy.call(this);
+		Ext.destroy(this.components);
+		this.components = [];
+	},
+	renderItems : function(startIndex, endIndex){
+		var ns = this.all.elements;
+		var args = [startIndex, 0];
+		for(var i = startIndex; i <= endIndex; i++){
 				var r = args[args.length] = [];
 				for(var items = this.items, j = 0, len = items.length, c; j < len; j++){
 				
 					// c = items[j].render ?
-					//	  c = items[j].cloneConfig() :
+					//	c = items[j].cloneConfig() :
 						
 						// RapidApp specific:
 						// Components are stored as serialized JSON to ensure they
 						// come out exactly the same every time:
 						c = Ext.create(Ext.decode(items[j]), this.defaultType);
-						  
-					 r[j] = c;
-					 if(c.renderTarget){
-						  c.render(Ext.DomQuery.selectNode(c.renderTarget, ns[i]));
-					 }else if(c.applyTarget){
-						  c.applyToMarkup(Ext.DomQuery.selectNode(c.applyTarget, ns[i]));
-					 }else{
-						  c.render(ns[i]);
-					 }
-					 
-					 if(Ext.isFunction(c.setValue) && c.applyValue){
-						  c.setValue(this.store.getAt(i).get(c.applyValue));
-						  c.on('blur', function(f){
+						
+					r[j] = c;
+					if(c.renderTarget){
+						c.render(Ext.DomQuery.selectNode(c.renderTarget, ns[i]));
+					}else if(c.applyTarget){
+						c.applyToMarkup(Ext.DomQuery.selectNode(c.applyTarget, ns[i]));
+					}else{
+						c.render(ns[i]);
+					}
+					
+					if(Ext.isFunction(c.setValue) && c.applyValue){
+						c.setValue(this.store.getAt(i).get(c.applyValue));
+						c.on('blur', function(f){
 							this.store.getAt(this.index).data[this.dataIndex] = f.getValue();
-						  }, {store: this.store, index: i, dataIndex: c.applyValue});
-					 }
-					 
+						}, {store: this.store, index: i, dataIndex: c.applyValue});
+					}
+					
 				}
-		  }
-		  this.components.splice.apply(this.components, args);
-	 },
-	 destroyItems : function(index){
-		  Ext.destroy(this.components[index]);
-		  this.components.splice(index, 1);
-	 },
+		}
+		this.components.splice.apply(this.components, args);
+	},
+	destroyItems : function(index){
+		Ext.destroy(this.components[index]);
+		this.components.splice(index, 1);
+	},
+	
+	add_record: function() {
+	
+		var Store = this.getStore();
+		
+		var recMaker = Ext.data.Record.create(Store.fields.items);
+		var newRec = new recMaker;
+		
+		if(! Store.api.create) {
+			Ext.Msg.alert('Cannot add','No create function has been defined');
+			return;
+		}
+		
+		return Store.add(newRec);
+	},
+	
+	
 	set_field_editable: function(editEl,fieldname,index,Record) {
 		
 		var dataWrap = editEl.child('div.data-wrapper');
