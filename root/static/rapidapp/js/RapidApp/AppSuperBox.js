c1d837581ac269785965797facb4522bd27e51df
Henry Van Styn <vanstyn@intellitree.com>
2011-05-04 04:44:54 -0400

    AppSuperBox: Initial working xtype 'appsuperbox' - preliminary
    New class Ext.ux.RapidApp.AppSuperBox extends Ext.ux.form.SuperBoxSelect
    and adds support for "Create New" clickable element within the combo
    list
    
    Preliminary work, see HOPS for example of how this is being used and
    implemented... The API and design will probably change quite a bit
    before this feature is completed...

--- root/static/rapidapp/js/RapidApp/AppSuperBox.js
+++ root/static/rapidapp/js/RapidApp/AppSuperBox.js
@@ -5,46 +5,28 @@ Ext.ux.RapidApp.AppSuperBox = Ext.extend(Ext.ux.form.SuperBoxSelect, {
 	
 	createItemClass: 'create-item',
 	
+	createItemHandler: null,
+	
 	onViewClick: function() {
 		var event = arguments[arguments.length - 1]; // <-- last passed argument, the event object;
 		var target = event.getTarget(null,null,true);
 		
 		// Handle create item instead of normal handler:
-		if (target.hasClass(this.createItemClass)) {
+		if (this.createItemHandler && target.hasClass(this.createItemClass)) {
 			this.collapse();
-			return this.createItemHandler.apply(this,arguments);
+			return this.createItemHandler.call(this,this.createItemCallback);
 		}
 
 		// Original handler:
 		Ext.ux.RapidApp.AppSuperBox.superclass.onViewClick.apply(this,arguments);
 	},
 	
-	createItemHandler: function() {
-		
-		console.dir('createItemHandler()');
-		
-		
-		var win = new Ext.Window({
-			title: "AppSuperBox ADD",
-			layout: 'fit',
-			width: 650,
-			height: 550,
-			closable: true,
-			modal: true,
-			items: {
-				xtype: 'autopanel',
-				autoLoad: {
-					url: '/main/explorer/organizations/add'
-				},
-				layout: 'fit'
-
-			}
-		});
-
-		win.show();
-
-		
-		
+	createItemCallback: function(data) {
+		var Store = this.getStore();
+		var recMaker = Ext.data.Record.create(Store.fields.items);
+		var newRec = new recMaker(data);
+		Store.insert(0,newRec);
+		this.addRecord(newRec);
 	}
 
 });
