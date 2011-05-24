d9dd11efb8967356e1d35b64d1fa596738d1690f
Henry Van Styn <vanstyn@intellitree.com>
2011-05-24 01:46:31 -0400

    AppCombo2: using replaceClass instead of addClass for valueCssField
    Fixed issue with classes applied from previous records remaining applied

--- root/static/rapidapp/js/RapidApp/AppCombo2.js
+++ root/static/rapidapp/js/RapidApp/AppCombo2.js
@@ -5,13 +5,18 @@
 */
 Ext.ns('Ext.ux.RapidApp.AppCombo2');
 Ext.ux.RapidApp.AppCombo2.ComboBox = Ext.extend(Ext.form.ComboBox,{
-
+	
+	lastValueClass: '',
+	
 	nativeSetValue: function(v) {
 		if (this.valueCssField) {
 			var record = this.findRecord(this.valueField, v);
 			if (record) {
 				var addclass = record.data[this.valueCssField];
-				if (addclass) { this.el.addClass(addclass); }
+				if (addclass) { 
+					this.el.replaceClass(this.lastValueClass,addclass);
+					this.lastValueClass = addclass;
+				}
 			}
 		}
 		return Ext.form.ComboBox.prototype.setValue.apply(this,arguments);
