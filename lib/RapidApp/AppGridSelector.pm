fb361de44432c08af6f69cebe48ec0ae9cd13f43
Henry Van Styn <vanstyn@intellitree.com>
2011-08-11 16:52:09 -0400

    AppGridSelector: Converted into a Role

--- lib/RapidApp/AppGridSelector.pm
+++ lib/RapidApp/AppGridSelector.pm
@@ -2,26 +2,6 @@ package RapidApp::AppGridSelector;
 use strict;
 use Moose;
 extends 'RapidApp::AppGrid2';
+with 'RapidApp::AppGrid2::Role::GridSelector';
 
-use RapidApp::Include qw(sugar perlutil);
-
-
-sub content {
-	my $self = shift;	
-	my $gridcnf = $self->SUPER::content;
-	
-	my $grid = RapidApp::JSONFunc->new( func => 'Ext.ComponentMgr.create', parm => $gridcnf );
-	
-	return {
-		xtype	=> 'appgridselector',
-		grid	=> $grid
-	};
-}
-
-
-
-
-
-#no Moose;
-#__PACKAGE__->meta->make_immutable;
 1;
