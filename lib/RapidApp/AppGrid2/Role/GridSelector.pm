adfc391dcdd0db3df19eb1816bca5f0b4db135d6
Henry Van Styn <vanstyn@intellitree.com>
2011-08-12 19:51:38 -0400

    GridSelector: automatically disable add/open records

--- lib/RapidApp/AppGrid2/Role/GridSelector.pm
+++ lib/RapidApp/AppGrid2/Role/GridSelector.pm
@@ -4,8 +4,12 @@ use Moose::Role;
 
 use RapidApp::Include qw(sugar perlutil);
 
+has 'add_record_class'	=> ( is => 'ro', default => undef );
+has 'open_record_class'	=> ( is => 'ro', default => undef );
+
 around 'content' => sub {
-	my ($orig, $self, @args) = @_;	
+	my ($orig, $self, @args) = @_;
+	
 	my $gridcnf = $self->$orig(@args);
 	
 	my $grid = RapidApp::JSONFunc->new( func => 'Ext.ComponentMgr.create', parm => $gridcnf );
