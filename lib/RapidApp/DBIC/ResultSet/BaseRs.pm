a55fea365cf862dac21b61fd7930cdbd645e57f6
Henry Van Styn <vanstyn@intellitree.com>
2011-09-23 16:57:07 -0400

    BaseRs: Removed 'native_rs'
    native_rs doesn't work as expected in many situations, so I just removed
    it. (see previous commit)

--- lib/RapidApp/DBIC/ResultSet/BaseRs.pm
+++ lib/RapidApp/DBIC/ResultSet/BaseRs.pm
@@ -10,16 +10,7 @@ use base 'DBIx::Class::ResultSet';
 # called from. Here we set custom attributes that will be passed 
 # along to all subsequent chained resultset objects that get created.
 # This allows us to apply the base_rs exactly one time without causing
-# a deep recursion loop. We also pass the original ResultSet object,
-# prior to applying base_rs, so we have the extra ability to get around
-# the base_rs restrictions if we need to using the special accessor
-# method native_rs. Because of the stacked design, with resultset's
-# being progressively filtered/limited with each new chained call, this 
-# is the only practical way to accomplish this. 
-#
-# The whole purpose of this is to be able to set useful defaults without 
-# having to call a custom/special search method in all locations, yet
-# still preserving a way to get to the original, unfiltered Rs.
+# a deep recursion loop. 
 #
 # This module is the successor to RapidApp::DBIC::ResultSet::BaseConditions,
 # accomplishing the same things as that module yet wihtout the need
@@ -27,8 +18,7 @@ use base 'DBIx::Class::ResultSet';
 # define an accessor method 'base_rs' in exactly the same way as any
 # custom search/resultset method accessor would be setup in a ResultSet
 # class, except instead of having to manually call base_rs, it becomes
-# the base/default, with access to the original provided by calling
-# native_rs.
+# the base/default
 
 # Define base_rs in the consuming ResultSet class. Should return a chained
 # ResultSet object, i.e. return $self->search_rs( $SEARCH, $ATTR );
@@ -42,14 +32,7 @@ sub search_rs {
 sub _get_apply_base_rs {
 	my $Rs = shift;
 	return $Rs if ($Rs->{attrs}->{_base_rs_applied});
-	$Rs = $Rs->SUPER::search_rs({},{ _base_rs_applied => 1 });
-	return $Rs->SUPER::search_rs({},{ _native_rs => $Rs })->base_rs;
-}
-
-sub native_rs {
-	my $self = shift;
-	my $Rs = $self->{attrs}->{_native_rs} || $self->search_rs->{attrs}->{_native_rs};
-	return $Rs;
+	return $Rs->SUPER::search_rs({},{ _base_rs_applied => 1 })->base_rs;
 }
 
 1;
