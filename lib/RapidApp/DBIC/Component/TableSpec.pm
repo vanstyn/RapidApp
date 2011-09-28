6cf3d62626f6ca1ce23eeabbad5261e0e319ec76
Henry Van Styn <vanstyn@intellitree.com>
2011-09-28 19:19:54 -0400

    TableSpec: IN PROGRESS work on 'TableSpec_setup_editor_dropdowns'

--- lib/RapidApp/DBIC/Component/TableSpec.pm
+++ lib/RapidApp/DBIC/Component/TableSpec.pm
@@ -49,6 +49,9 @@ sub TableSpec_setup_editor_dropdowns {
 	my $self = shift;
 	foreach my $colspec (@_) { 
 	
+		my $colname = $colspec;
+		$colname =~ s/\./\_/g;
+	
 		my ($rel,$col) = split(/\./,$colspec,2);
 		
 		die "Invalid colspec '$colspec'" unless ($col);
@@ -56,11 +59,67 @@ sub TableSpec_setup_editor_dropdowns {
 		
 		scream($info);
 		
-		#print STDERR CYAN . BOLD . Dumper
-	
-	
-	
+		my $foreign_col = $self->get_foreign_column_from_cond($info->{cond});
+		
+		# This coderef gets called later, after the RapidApp
+		# Root Module has been loaded.
+		rapidapp_add_global_init_coderef( sub { 
+			my $rootModule = shift;
+			$rootModule->apply_init_modules( tablespec => 'RapidApp::AppBase' ) 
+				unless ( $rootModule->has_module('tablespec') );
+			
+			my $TableSpecModule = $rootModule->Module('tablespec');
+
+			my $c = RapidApp::ScopedGlobals->get('catalystClass');
+			
+			my $Source = $c->model('DB')->source($info->{source});
+			
+			
+			my $module_name = $self->table . '_' . $colname;
+			$TableSpecModule->apply_init_modules(
+				$module_name => {
+					class	=> 'RapidApp::DbicAppCombo',
+					params	=> {
+						valueField    => ($Source->primary_columns)[0],
+						name          => $colname,
+						ResultSource  => $Source,
+					}
+				}
+			);
+			my $Module = $TableSpecModule->Module($module_name);
+			
+			
+			# TODO: apply the config to the Column object...
+			
+			scream(ref($Module));
+			
+			#my $Module = $rootModule
+			
+			#scream($rootModule);
+		
+		
+		
+		});	
 	}
+}
+
+# TODO: Find a better way to handle this. Is there a real API
+# in DBIC to find this information?
+sub get_foreign_column_from_cond {
+	my $self = shift;
+	my $cond = shift;
+	
+	die "currently only single-key hashref conditions are supported" unless (
+		ref($cond) eq 'HASH' and
+		scalar keys %$cond == 1
+	);
+	
+	foreach my $i (%$cond) {
+		my ($side,$col) = split(/\./,$i);
+		return $col if (defined $col and $side eq 'foreign');
+	}
+	
+	die "Failed to find forein column from condition: " . Dumper($cond);
 }
 
 
