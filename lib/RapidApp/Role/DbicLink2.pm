c70e7ba9951d6703f4ac7d4c3838afa6b2909978
Henry Van Styn <vanstyn@intellitree.com>
2011-10-19 18:47:06 -0400

    DbicLink2: Added 'multifilter' support

--- lib/RapidApp/Role/DbicLink2.pm
+++ lib/RapidApp/Role/DbicLink2.pm
@@ -4,6 +4,7 @@ use Moose::Role;
 
 use RapidApp::Include qw(sugar perlutil);
 use RapidApp::TableSpec::Role::DBIC;
+use Clone qw(clone);
 
 
 # Colspec attrs can be specified as simple arrayrefs
@@ -64,7 +65,7 @@ sub read_records {
 	#$Rs->search_rs({},$self->req_Rs_Attr_spec($params));
 
 	# Apply multifilter:
-	#$Rs = $Rs->search_rs
+	$Rs = $self->chain_Rs_req_multifilter($Rs,$params);
 	
 	
 	
@@ -130,7 +131,9 @@ sub chain_Rs_req_base_Attr {
 	return $Rs->search_rs({},$attr);
 }
 
-=pod
+
+
+
 # Applies multifilter search to ResultSet:
 sub chain_Rs_req_multifilter {
 	my $self = shift;
@@ -139,91 +142,66 @@ sub chain_Rs_req_multifilter {
 	
 	my $multifilter = $self->param_decodeIf($params->{multifilter}) or return $Rs;
 	
-	return $Rs;
-	
-	return $Rs unless (defined $params->{multifilter});
-	
-		my $multifilter = JSON::PP::decode_json($params->{multifilter});
-	
-		my $map_dbfnames;
-		$map_dbfnames = sub {
-			my $multi = shift;
-			if(ref($multi) eq 'HASH') {
-				return $map_dbfnames->($multi->{'-and'}) if (defined $multi->{'-and'});
-				return $map_dbfnames->($multi->{'-or'}) if (defined $multi->{'-or'});
-				
-				foreach my $f (keys %$multi) {
-					# Not sure why this is commented out.
-					# next unless $self->search_permitted($f);
-					
-					# convert table column name to db field name
-					my $dbfName = $self->ExtNamesToDbFields->{$f};
-					if (!defined $dbfName) {
-						$self->c->log->error("Client supplied Unknown multifilter-field '$f' in Ext Query!");
-						next;
-					}
-					
-					$multi->{$dbfName} = $multi->{$f};
-					delete $multi->{$f};
-					
-					# --- translate special content conditions to "LIKE" conditions
-					if (defined $multi->{$dbfName}->{contains}) {
-						$multi->{$dbfName}->{like} = '%' . $multi->{$dbfName}->{contains} . '%';
-						delete $multi->{$dbfName}->{contains};
-					}
-					
-					if (defined $multi->{$dbfName}->{starts_with}) {
-						$multi->{$dbfName}->{like} = $multi->{$dbfName}->{starts_with} . '%';
-						delete $multi->{$dbfName}->{starts_with};
-					}
-					
-					if (defined $multi->{$dbfName}->{ends_with}) {
-						$multi->{$dbfName}->{like} = '%' . $multi->{$dbfName}->{ends_with};
-						delete $multi->{$dbfName}->{ends_with};
-					}
-					
-					if (defined $multi->{$dbfName}->{not_contain}) {
-						$multi->{$dbfName}->{'not like'} = '%' . $multi->{$dbfName}->{not_contain} . '%';
-						delete $multi->{$dbfName}->{not_contain};
-					}
-					# ---
-					
-					# -- Add '%' characters to "like" searches: (disabled in favor of special content conditions above)
-					#if (defined $multi->{$dbfName}->{like} and not $multi->{$dbfName}->{like} =~ /\%/) {
-					#	$multi->{$dbfName}->{like} = '%' . $multi->{$dbfName}->{like} . '%';
-					#}
-					# --
+	my $attr = { join => {} };
+	my $cond = $self->multifilter_to_dbf($multifilter,$attr->{join});
 
-				}
-			}
-			elsif(ref($multi) eq 'ARRAY') {
-				foreach my $item (@$multi) {
-					$map_dbfnames->($item);
-				}
-			}
-		};
+	return $Rs->search_rs($cond,$attr);
+}
+
+sub multifilter_to_dbf {
+	my $self = shift;
+	my $multi = clone(shift);
+	my $join = shift || {};
+	
+	return $self->multifilter_to_dbf({ '-and' => $multi },$join) if (ref($multi) eq 'ARRAY');
+	
+	die 'Invalid multifilter' unless (ref($multi) eq 'HASH');
+	
+	foreach my $f (keys %$multi) {
+		if($f eq '-and' or $f eq '-or') {
+			die "-and/-or must reference an ARRAY/LIST" unless (ref($multi->{$f}) eq 'ARRAY');
+			my $new = [];
+			push @$new, $self->multifilter_to_dbf($_,$join) for (@{$multi->{$f}});
+			$multi->{$f} = $new;
+			next;
+		}
 		
-		$map_dbfnames->($multifilter);
-	
-	
-		# Recursive sub to make all lists explicitly '-and' lists:
-		my $add_ands;
-		$add_ands = sub {
-			my $multi = shift;
-			return $multi unless (ref($multi) eq 'ARRAY');
+		my $dbfName = $self->TableSpec->resolve_dbic_colname($f,$join);
 			
-			foreach my $item (@$multi) {
-				$item = $add_ands->($item);
-			}
-			return { '-and' => $multi };
-		};
+		if (!defined $dbfName) {
+			$self->c->log->error("Client supplied Unknown multifilter-field '$f' in Ext Query!");
+			next;
+		}
 		
-		push @$filter_search, $add_ands->($multifilter);
+		$multi->{$dbfName} = $multi->{$f};
+		delete $multi->{$f};
+		
+		# --- translate special content conditions to "LIKE" conditions
+		if (defined $multi->{$dbfName}->{contains}) {
+			$multi->{$dbfName}->{like} = '%' . $multi->{$dbfName}->{contains} . '%';
+			delete $multi->{$dbfName}->{contains};
+		}
+		
+		if (defined $multi->{$dbfName}->{starts_with}) {
+			$multi->{$dbfName}->{like} = $multi->{$dbfName}->{starts_with} . '%';
+			delete $multi->{$dbfName}->{starts_with};
+		}
+		
+		if (defined $multi->{$dbfName}->{ends_with}) {
+			$multi->{$dbfName}->{like} = '%' . $multi->{$dbfName}->{ends_with};
+			delete $multi->{$dbfName}->{ends_with};
+		}
+		
+		if (defined $multi->{$dbfName}->{not_contain}) {
+			$multi->{$dbfName}->{'not like'} = '%' . $multi->{$dbfName}->{not_contain} . '%';
+			delete $multi->{$dbfName}->{not_contain};
+		}
 	}
 	
-	
+	return $multi;
 }
-=cut
+
+
 
 sub param_decodeIf {
 	my $self = shift;
