68a2ef927455bf368fb83bafe9ec23dff10bdc70
Henry Van Styn <vanstyn@intellitree.com>
2011-11-01 17:23:21 -0400

    Added DbicAppGrid3
    Just like DbicAppGrid2 but uses DbicLink2 instead of DbicLink. Sets the
    xtype to 'appgrid2ed' is there are updatable columns

---
+++ lib/RapidApp/DbicAppGrid3.pm
@@ -0,0 +1,49 @@
+package RapidApp::DbicAppGrid3;
+
+
+use strict;
+use Moose;
+extends 'RapidApp::AppGrid2';
+with 'RapidApp::Role::DbicLink2';
+
+#use RapidApp::MooseX::ClassAttrSugar;
+#setup_apply_methods_for('config');
+#setup_apply_methods_for('listeners');
+
+
+#apply_default_config(
+#	remote_columns		=> \1,
+#	loadMask				=> \1
+#
+#);
+
+#sub BUILD {
+#	my $self = shift;
+#	$self->apply_config(
+#		remote_columns		=> \1,
+#		loadMask				=> \1
+#	);
+#}
+
+sub BUILD {
+	my $self = shift;
+	
+	if ($self->updatable_colspec) {
+		$self->apply_extconfig( xtype => 'appgrid2ed' );
+		$self->apply_extconfig( clicksToEdit => 1 );
+	}
+	
+}
+
+
+
+
+
+
+
+#### --------------------- ####
+
+
+no Moose;
+#__PACKAGE__->meta->make_immutable;
+1;
