vanstyn/RapidApp

commit 5f365fcdcf89ef37b2dded7f1f95170bc46b183e

MultiFilter: minor tweak, fixes blank date load problem

root/static/rapidapp/js/MultiFilter.js
@@ -18,16 +18,14 @@ Ext.ux.MultiFilter.Plugin = Ext.extend(Ext.util.Observable,{
 		}
 		
 		
-		
 		this.store.on('beforeload',function(store,options) {
 			if(store.filterdata) {
-				var encoded = Ext.encode(store.filterdata);
 				Ext.apply(options.params, {
-					'multifilter': encoded 
+					'multifilter': this.getMultiFilterParam() 
 				});
 			}
 			return true;
-		});
+		},this);
 	
 		if (grid.rendered) {
 			this.onRender();
@@ -39,7 +37,14 @@ Ext.ux.MultiFilter.Plugin = Ext.extend(Ext.util.Observable,{
 			 });
 		}
 	},
+	
+	getMultiFilterParam: function() {
 		
+		var data = this.store.filterdata;
+		
+		return Ext.encode(data);
+	},
+	
 	onRender: function() {
 	
 		this.filtersBtn = new Ext.Button({
@@ -500,7 +505,6 @@ Ext.ux.MultiFilter.Criteria = Ext.extend(Ext.Container,{
 			this.reverseFieldNameMap[val] = key;
 		},this);
 		
-		
 		return this.columnMap;
 	},
 	
@@ -593,7 +597,7 @@ Ext.ux.MultiFilter.Criteria = Ext.extend(Ext.Container,{
 	getData: function() {
 		var field = this.getComponent('field_combo').getRawValue();
 		var cond = this.getComponent('cond_combo').getRawValue();
-		var val = this.getComponent('datafield').getValue();
+		var val = this.getComponent('datafield').getRawValue();
 
 		
 		//field combo
@@ -603,7 +607,7 @@ Ext.ux.MultiFilter.Criteria = Ext.extend(Ext.Container,{
 		
 		
 		
-		// -------------
+		// --- translate relationship column to its id *or* render col ---
 		var column = this.columnMap[field];
 		if(column) {
 			
@@ -614,7 +618,7 @@ Ext.ux.MultiFilter.Criteria = Ext.extend(Ext.Container,{
 				if(column.query_search_use_column) { field = column.query_search_use_column; }
 			}
 		}
-		// -------------
+		// --- ---
 		
 		
 		
@@ -626,7 +630,7 @@ Ext.ux.MultiFilter.Criteria = Ext.extend(Ext.Container,{
 		var data = {};
 		data[field] = {};
 		data[field][cond] = val;
-		
+			
 		return data;
 	},
 	

commit d4b8d62d0807942114ed26925882db00f6b9d09b

Fixed regression from 5f365fcdcf89ef37b2dded7f1f95170bc46b183e (Apr 1)
But now the problem that regression was trying to solve is back... need
to come up with a different solution...

root/static/rapidapp/js/MultiFilter.js
@@ -737,7 +737,7 @@ Ext.ux.MultiFilter.Criteria = Ext.extend(Ext.Container,{
 		
 		var field = field_combo ? field_combo.getRawValue() : null,
 			cond = cond_combo ? cond_combo.getRawValue() : null,
-			val = datafield ? datafield.getRawValue() : null;
+			val = datafield ? datafield.getValue() : null;
 		
 		if(!field || !cond) { return null; }
 		