c85822125bd1b5e191492262b427c98bc6ee21e0
Henry Van Styn <vanstyn@intellitree.com>
2012-08-04 06:37:40 -0400

    RapidApp::Role::CatalystApplication now declared in RapidApp::Catalyst
    RapidApp::Role::CatalystApplication no longer needs to be supplied in
    app configs

--- lib/RapidApp/Catalyst.pm
+++ lib/RapidApp/Catalyst.pm
@@ -1,9 +1,14 @@
 package RapidApp::Catalyst;
-use Catalyst qw(+RapidApp::CatalystX::SimpleCAS);
+
+# Built-in plugins required for all RapidApp Applications:
+use Catalyst qw(
+	+RapidApp::Role::CatalystApplication
+	+RapidApp::CatalystX::SimpleCAS	
+);
+
 use base 'Catalyst';
 
 use RapidApp::AttributeHandlers;
-
 use RapidApp::Include qw(sugar perlutil);
 
 
