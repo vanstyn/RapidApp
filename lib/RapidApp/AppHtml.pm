35eae11b51d1af52aef6a8a1b4af84a46fe00c21
Henry Van Styn <vanstyn@intellitree.com>
2012-09-24 20:33:27 -0400

    Added RapidApp::AppHtml

---
+++ lib/RapidApp/AppHtml.pm
@@ -0,0 +1,42 @@
+package RapidApp::AppHtml;
+use strict;
+use Moose;
+extends 'RapidApp::AppCmp';
+
+use RapidApp::Include qw(sugar perlutil);
+
+# This class exists just to properly setup a container with correct scrollbars, 
+# in all browsers (including IE), to contain simple HTML. Derived class should
+# provide an "html" method that will return the html content, called on each
+# request.
+
+# but yet it still doesn't work right with dynamic content in IE
+
+# here is a trick for the html:
+#<div style="position:absolute; left: 0px;right:25px;">
+
+
+sub BUILD {
+	my $self = shift;
+	
+	$self->apply_extconfig(
+		xtype => 'panel',
+		layout => 'anchor',
+		autoScroll => \1,
+	);
+}
+
+sub html { die "Virtual Method!" } 
+
+around 'content' => sub {
+	my $orig = shift;
+	my $self = shift;
+	
+	my $content = $self->$orig(@_);
+	
+	$content->{html} = $self->html;
+	
+	return $content;
+};
+
+1;
