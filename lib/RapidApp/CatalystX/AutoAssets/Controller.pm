c3a52ac051eb90445491d1f7b60ce3fda19210d9
Henry Van Styn <vanstyn@intellitree.com>
2012-10-17 23:08:57 -0400

    AutoAssets: MAJOR REFACTORING - locking + fingerprint checks

--- lib/RapidApp/CatalystX/AutoAssets/Controller.pm
+++ lib/RapidApp/CatalystX/AutoAssets/Controller.pm
@@ -25,16 +25,33 @@ use Switch qw(switch);
 use IO::File;
 use IO::All;
 use Digest::SHA1;
+use File::Path qw(make_path remove_tree);
+use Fcntl qw( :DEFAULT :flock :seek F_GETFL );
 
 require JavaScript::Minifier;
 require CSS::Minifier;
 
 has 'minify', is => 'ro', isa => 'Bool', default => 0;
 
-has 'built_dir', is => 'ro', lazy => 1, default => sub {
+has 'work_dir', is => 'ro', lazy => 1, default => sub {
 	my $self = shift;
 	my $c = $self->_app;
-	return $c->config->{home} . '/built_assets';
+	return $c->config->{home} . '/root/autoassets';
+};
+
+has 'built_dir', is => 'ro', lazy => 1, default => sub {
+	my $self = shift;
+	return $self->work_dir . '/_built';
+};
+
+has 'built_fingerprint_file', is => 'ro', lazy => 1, default => sub {
+	my $self = shift;
+	return $self->work_dir . '/.built_fingerprint';
+};
+
+has 'build_lock_file', is => 'ro', lazy => 1, default => sub {
+	my $self = shift;
+	return $self->work_dir . '/.lockfile';
 };
 
 has 'extjs_theme_css', is => 'ro', lazy => 1, isa => 'Str', default => sub {
@@ -80,22 +97,11 @@ has 'css_asset_path', is => 'rw', isa => 'Maybe[Str]', default => undef;
 
 sub BUILD {
 	my $self = shift;
-	$self->init_built_dir;
-	$self->clean_built_dir;
-	$self->failsafe_init_check_built_dir;
+	$self->init_work_dir;
 }
 
-# In case the wrong built_dir was accidently specified, abort (because all files will be deleted)
-sub failsafe_init_check_built_dir {
-	my $self = shift;
-	die "Unexpected existing files detected in built_dir, aborting for safety. " .
-	 "Please manually remove the files in '" . $self->built_dir . "' or specify a different directory"
-		if(scalar $self->get_built_dir_files > 2);
-};
 
-
-
-# safe for glob() if built_dir contains whitespace
+## safe for glob() if built_dir contains whitespace
 has 'safe_built_dir', is => 'ro', lazy => 1, default => sub {
 	my $self = shift;
 	my $dir = $self->built_dir;
@@ -103,10 +109,13 @@ has 'safe_built_dir', is => 'ro', lazy => 1, default => sub {
 	return $dir;
 };
 
-sub init_built_dir {
+
+sub init_work_dir {
 	my $self = shift;
 	return if (-d $self->built_dir);
-	mkdir $self->built_dir or die "Failed to create directory: " . $self->built_dir;
+	my $err;
+	make_path($self->built_dir, {error => \$err});
+	die join("\n",@$err) if (scalar @$err > 0);
 }
 
 # List of files currently in the built dir
@@ -117,41 +126,116 @@ sub get_built_dir_files {
 
 # Conservative clean up of built_dir. Only delete files that 
 # were (probably) created by us:
-sub clean_built_dir {
-	my $self = shift;
-	unlink $_ for( glob(
-		$self->safe_built_dir . '/*.js ' .
-		$self->safe_built_dir . '/*.css ' .
-		$self->safe_built_dir . '/*.js.tmp ' .
-		$self->safe_built_dir . '/*.css.tmp'
-	));
-	
-	# this should only be called during BUILD, but should it be called
-	# later on for some reason, make sure the assets will be generated
-	# on the next request:
-	$self->prepared_inc_mtime_concat(undef);
-}
+#sub clean_built_dir {
+#	my $self = shift;
+#	unlink $_ for( glob(
+#		$self->safe_built_dir . '/*.js ' .
+#		$self->safe_built_dir . '/*.css ' .
+#		$self->safe_built_dir . '/*.js.tmp ' .
+#		$self->safe_built_dir . '/*.css.tmp'
+#	));
+#	
+#	# this should only be called during BUILD, but should it be called
+#	# later on for some reason, make sure the assets will be generated
+#	# on the next request:
+#	$self->inc_mtimes(undef);
+#}
+
 
 sub get_inc_files {
 	my $self = shift;
 	return (glob($self->inc));
 }
 
-has 'prepared_inc_mtime_concat', is => 'rw', isa => 'Maybe[Str]', default => undef;
+has 'max_fingerprint_calc_age', is => 'ro', isa => 'Int', default => (60*60*12); # 12 hours
+has 'last_fingerprint_calculated', is => 'rw', isa => 'Maybe[Int]', default => undef;
+
+has 'built_mtime', is => 'rw', isa => 'Maybe[Str]', default => undef;
+sub get_built_mtime {
+	my $self = shift;
+	$self->init_work_dir;
+	return xstat($self->built_dir)->{mtime};
+}
+
+has 'inc_mtimes', is => 'rw', isa => 'Maybe[Str]', default => undef;
 sub get_inc_mtime_concat {
 	my $self = shift;
 	my $list = shift || [ $self->get_inc_files ];
 	return join('-', map { xstat($_)->{mtime} } @$list );
 }
 
+
+sub calculate_fingerprint {
+	my $self = shift;
+	my @built = $self->get_built_dir_files;
+	return undef unless (scalar @built == 2); #<-- there should be exactly 2 built files
+	my $sha1 = $self->file_checksum($self->get_inc_files,@built);
+	$self->last_fingerprint_calculated(time) if ($sha1);
+	return $sha1;
+}
+
+sub current_fingerprint {
+	my $self = shift;
+	return undef unless (-f $self->built_fingerprint_file);
+	my $fingerprint = io($self->built_fingerprint_file)->slurp;
+	return $fingerprint;
+}
+
+sub save_fingerprint {
+	my $self = shift;
+	my $fingerprint = shift or die "Expected fingerprint/checksum argument";
+	return io($self->built_fingerprint_file)->print($fingerprint);
+}
+
+sub calculate_save_fingerprint {
+	my $self = shift;
+	my $fingerprint = $self->calculate_fingerprint or return 0;
+	return $self->save_fingerprint($fingerprint);
+}
+
+sub fingerprint_calc_current {
+	my $self = shift;
+	my $last = $self->last_fingerprint_calculated or return 0;
+	return 1 if (time - $last < $self->max_fingerprint_calc_age);
+	return 0;
+}
+
 sub prepare_assets {
 	my $self = shift;
 	
 	my @files = $self->get_inc_files;
-	my $mtime_concat = $self->get_inc_mtime_concat(\@files);
+	my $inc_mtimes = $self->get_inc_mtime_concat(\@files);
+	my $built_mtime = $self->get_built_mtime;
 	
-	# No new changes, return
-	return if ($self->prepared_inc_mtime_concat eq $mtime_concat);
+	# Check cached mtimes to see if anything has changed. This is a lighter
+	# first pass check than the fingerprint check which calculates a sha1 for
+	# all the source files and existing built files
+	return if (
+		$self->fingerprint_calc_current &&
+		$self->inc_mtimes eq $inc_mtimes && 
+		$self->built_mtime eq $built_mtime
+	);
+	
+	# --- Blocks for up to 2 minutes waiting to get an exclusive lock or dies
+	$self->get_build_lock;
+	# ---
+	
+	# Check the fingerprint:
+	my $fingerprint = $self->calculate_fingerprint;
+	if($fingerprint && $self->current_fingerprint eq $fingerprint) {
+		# If the mtimes changed but the fingerprint matches we don't need to regenerate. 
+		# This will happen if another process just built the files while we were waiting 
+		# for the lock and on the very first time after the application starts up
+		$self->inc_mtimes($inc_mtimes);
+		$self->built_mtime($built_mtime);
+		$self->find_set_unknown_asset_paths; #<-- only applies to first call when assets are already built
+		return $self->release_build_lock;
+	}
+	
+	# Need to do a rebuild:
+	
+	# remove any old built files:
+	remove_tree( $self->built_dir, {keep_root => 1} );
 	
 	# separate css from js files and ignore all other files:
 	my @js = ();
@@ -159,21 +243,18 @@ sub prepare_assets {
 	($_ =~ /\.js$/ and push @js, $_) or ($_ =~ /\.css$/ and push @css, $_)
 		for (@files);
 	
+	# -- Do the actual building:
 	$self->js_asset_path( $self->generate_asset('js',\@js) );
 	$self->css_asset_path( $self->generate_asset('css',\@css) );
+	# --
 	
-	# Clean other files from the built_dir:
-	my @unlinks = grep {
-		$_ ne $self->js_asset_path and
-		$_ ne $self->css_asset_path and
-		! (-d $_) and			# exclude directories (should never happen)
-		! ($_ =~ /\.tmp$/)		# exclude tmp files to avoid clobbering files being written by other procs
-	} $self->get_built_dir_files;
-	unlink $_ for(@unlinks);
+	# Update the fingerprint (global) and cached mtimes (specific to the current process)
+	$self->inc_mtimes($inc_mtimes);
+	$self->built_mtime($self->get_built_mtime);
+	$self->calculate_save_fingerprint;
 	
-	# Update mtime_concat
-	# TODO: store this in a FastMmap so each process doesn't regenerate the files
-	$self->prepared_inc_mtime_concat($mtime_concat);
+	# Release the lock and return:
+	return $self->release_build_lock;
 }
 
 
@@ -184,8 +265,8 @@ sub generate_asset {
 	
 	die '$ext may only be js or css!!' unless ($ext eq 'js' || $ext eq 'css');
 	
-	$self->init_built_dir;
-	my $tmpf = $self->built_dir . '/-' . $$ . '-' . rand . '.' . $ext . '.tmp';
+	$self->init_work_dir;
+	my $tmpf = $self->built_dir . '/-' . $ext . '.tmp';
 	
 	my $fd = IO::File->new($tmpf, '>:raw') or die $!;
 	if($self->minify) {
@@ -221,15 +302,18 @@ sub generate_asset {
 
 sub file_checksum {
 	my $self = shift;
-	my $file = shift;
+	my @files = @_;
 	
-	my $FH = IO::File->new();
-	$FH->open('< ' . $file) or die "$! : $file\n";
-	$FH->binmode;
+	my $Sha1 = Digest::SHA1->new;
+	foreach my $file (@files) {
+		my $FH = IO::File->new();
+		$FH->open('< ' . $file) or die "$! : $file\n";
+		$FH->binmode;
+		$Sha1->addfile($FH);
+		$FH->close;
+	}
 	
-	my $sha1 = Digest::SHA1->new->addfile($FH)->hexdigest;
-	$FH->close;
-	return $sha1;
+	return $Sha1->hexdigest;
 }
 
 
@@ -330,5 +414,63 @@ sub html_head_includes {
 		) .
 		"\r\n<!--  ---- END AUTO GENERATED ASSETS ----  -->\r\n\r\n";
 }
+
+# Needed if current assets are already built but we don't know what they are:
+sub find_set_unknown_asset_paths {
+	my $self = shift;
+	return if ($self->css_asset_path && $self->js_asset_path);
+	my @built = $self->get_built_dir_files;
+	foreach my $path (@built) {
+		$self->css_asset_path( $path ) if ($path =~ /\.css$/);
+		$self->js_asset_path( $path ) if ($path =~ /\.js$/);
+	}
+}
+
+
+has 'max_lock_wait', is => 'ro', isa => 'Int', default => 120;
+
+sub get_build_lock_wait {
+	my $self = shift;
+	my $start = time;
+	until($self->get_build_lock) {
+		my $elapsed = time - $start;
+		die "AutoAssets: aborting waiting for lock after $elapsed"
+			if ($elapsed >= $self->max_lock_wait);
+		sleep 1;
+	}
+}
+
+sub get_build_lock {
+	my $self = shift;
+	my $fname = $self->build_lock_file;
+	sysopen(LOCKHANDLE, $fname, O_RDWR|O_CREAT|O_EXCL, 0644)
+		or sysopen(LOCKHANDLE, $fname, O_RDWR)
+		or die "Unable to create or open $fname\n";
+	fcntl(LOCKHANDLE, F_SETFD, FD_CLOEXEC) or die "Failed to set close-on-exec for $fname";
+	my $lockStruct= pack('sslll', F_WRLCK, SEEK_SET, 0, 0, $$);
+	if (fcntl(LOCKHANDLE, F_SETLK, $lockStruct)) {
+		my $data= "$$";
+		syswrite(LOCKHANDLE, $data, length($data)) or die "Failed to write pid to $fname";
+		truncate(LOCKHANDLE, length($data)) or die "Failed to resize $fname";
+		# we do not close the file, so that we maintain the lock.
+		return 1;
+	}
+	$self->release_build_lock;
+	return 0;
+}
+
+sub release_build_lock {
+	my $self = shift;
+	close LOCKHANDLE;
+}
+
+sub end :Private {
+	my ($self,$c) = @_;
+	# Make sure we never keep a build lock past the end of a request:
+	$self->release_build_lock;
+}
+
+
+
 
 1;
