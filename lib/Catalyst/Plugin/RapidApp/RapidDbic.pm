73d301a584e6d732335dd35c98a3f574e7101eab
Henry Van Styn <vanstyn@intellitree.com>
2013-05-26 12:35:36 -0400

    RapidDbic: Fixed bug in internally generated links

--- lib/Catalyst/Plugin/RapidApp/RapidDbic.pm
+++ lib/Catalyst/Plugin/RapidApp/RapidDbic.pm
@@ -177,7 +177,7 @@ before 'setup_component' => sub {
     # just a stop-gap until this functionality is factored into the RapidApp API 
     # officially, somehow...
     my $module_name = lc($model_name . '_' . $class->table);
-    my $grid_url = '/main/navtree/' . $module_name;
+    my $grid_url = '/main/db/' . $module_name;
     $class->TableSpec_set_conf(
       priority_rel_columns => 1,
       open_url_multi => $grid_url,
