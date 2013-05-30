9e552b9814a2c6263d67cd24071f02cc0f59d0ac
Henry Van Styn <vanstyn@intellitree.com>
2013-05-30 08:44:36 -0400

    AppPageViewer: supports parsing <title> for setting tabTitle

--- lib/RapidApp/AppPageViewer.pm
+++ lib/RapidApp/AppPageViewer.pm
@@ -7,9 +7,11 @@ use RapidApp::Include qw(sugar perlutil);
 
 # Module allows viewing pages in a tab by file name
 
+use HTML::TokeParser::Simple;
 use Switch qw(switch);
 
 has 'content_dir', is => 'ro', isa => 'Str', required => 1;
+has 'parse_title', is => 'ro', isa => 'Bool', default => 1;
 
 sub _requested_file {
   my $self = shift;
@@ -39,18 +41,44 @@ sub html {
     tabIconCls => 'icon-document'
   );
   
+  my $content;
+  
   switch(lc($ext)) {
     case('tt') {
       my $vars = { c => $self->c };
-      return $self->c->template_render($path,$vars);
+      $content = $self->c->template_render($path,$vars);
     }
+    ##
+    ## TODO: may support non-templates in the future
+    
     else {
       die usererr "Cannot display $file - unknown file extention type '$ext'", 
         title => "Unknown file type"
     }
   }
+  
+  my $title = $self->parse_title ? $self->_parse_get_title(\$content) : undef;
+  $self->apply_extconfig(
+    tabTitle => '<span style="color:darkgreen;">' . $title . '</span>',
+  ) if ($title);
+  
+  return $content;
 
-} 
+}
+
+sub _parse_get_title {
+  my $self = shift;
+  my $htmlref = shift;
+  
+  # quick/simple: return the inner text of the first <title> tag seen
+  my $parser = HTML::TokeParser::Simple->new($htmlref);
+  while (my $tag = $parser->get_tag) {
+    return $parser->get_token->as_is if($tag->is_tag('title')); 
+  }
+
+  return undef;
+}
+
 
 
 1;
