3a433351a235e7ea3eb655a6d4d0867026e0466f
Henry Van Styn <vanstyn@intellitree.com>
2013-07-31 16:27:29 -0400

    AppTemplateTree: misc improvements, async loading, etc

--- lib/RapidApp/AppTemplateTree.pm
+++ lib/RapidApp/AppTemplateTree.pm
@@ -15,43 +15,61 @@ system (RapidApp::Template::*)
 
 use RapidApp::Include qw(sugar perlutil);
 
-has '+fetch_nodes_deep', default => 1;
+has '+fetch_nodes_deep', default => 0;
 has 'template_regex', is => 'ro', isa => 'Maybe[Str]', default => sub {undef};
-
-sub template_list {
-  my $self = shift;
-  my $TC = $self->c->template_controller;
-  return $TC->get_Provider->list_templates($self->template_regex);
-}
 
 sub template_tree_items {
 	my $self = shift;
   
-  my $templates = $self->template_list;
+  my $TC = $self->c->template_controller;
+  my $templates = $TC->get_Provider->list_templates($self->template_regex);
   
-  return [ map {{
-    id => 'tpl-' . $_,
-    leaf => \1,
-    text => $_,
-    iconCls => 'ra-icon-page-white-world',
-    href => '#!/tple/' . $_,
-    loaded => \1
-  }} @$templates ];
+  my $items = [];
+  foreach my $template (@$templates) {
+    my $cnf = {
+      id => 'tpl-' . $template,
+      leaf => \1,
+      text => $template,
+      iconCls => 'ra-icon-page-white-world',
+      href => '#!/tple/' . $template,
+      loaded => \1
+    };
+    
+    # Show 'external' templates differently:
+    if($TC->Access->template_external_tpl($template)) {
+      $cnf->{iconCls} = 'ra-icon-page-white';
+      $cnf->{text} = join('',
+        '<span style="color:purple;">',
+        $cnf->{text},
+        '</span>'
+      )
+    }
+    
+    push @$items, $cnf;
+  }
+  
+  return $items;
 }
 
 
-sub TreeConfig {
-  my $self = shift;
+
+sub fetch_nodes {
+	my $self = shift;
+	my ($node) = @_;
   
-  my $cnf = [{
+  # Return the root node without children to spare the
+  # template query until it is actually expanded:
+  return [{
 		id			=> 'tpl-list',
 		text		=> 'Templates',
 		expand		=> 0,
-		children	=> $self->template_tree_items
-	}];
+	}] if ($node eq 'root');
   
-  return $cnf;
+	# The only other possible request is for the children of 
+  # 'root/tpl-list' above:
+	return $self->template_tree_items;
 }
+
 
 
 1;
