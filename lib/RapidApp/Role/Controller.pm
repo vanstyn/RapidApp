c3e7b2784eedfeef9f7c613e7a8cbb9b4910ad00
Henry Van Styn <vanstyn@intellitree.com>
2013-08-09 15:15:26 -0400

    Role::Controller: direct browser auto redirect more selective
    The auto redirect to '#!' url is now more selective; it only applies to
    paths that are verified as submodules or actions

--- lib/RapidApp/Role/Controller.pm
+++ lib/RapidApp/Role/Controller.pm
@@ -226,16 +226,19 @@ sub Controller {
   ### -----------------
   ### NEW: detect direct browser GET requests (i.e. not from the ExtJS client):
   ### and redirect them back to the #! hashnav path
-  if ($c->req->method eq 'GET' && ! $c->req->header('X-RapidApp-RequestContentType') && scalar(@args) > 0) {
-    my $url = join('/','/#!',@args);
-    my %params = %{$c->req->params};
-    if(keys %params > 0) {
-      my $qs = join('&',map { $_ . '=' . uri_escape($params{$_}) } keys %params);
-      $url .= '?' . $qs;
+  my ($opt) = @args;
+  if ($opt && $c->req->method eq 'GET' && ! $c->req->header('X-RapidApp-RequestContentType')) {
+    # Only for paths which are actually registered modules/actions:
+    if($self->_load_module($opt) || $self->has_action($opt)) {
+      my $url = join('/','/#!',@args);
+      my %params = %{$c->req->params};
+      if(keys %params > 0) {
+        my $qs = join('&',map { $_ . '=' . uri_escape($params{$_}) } keys %params);
+        $url .= '?' . $qs;
+      }
+      $c->response->redirect($url);
+      return $c->detach;
     }
-    
-    $c->response->redirect($url);
-    return $c->detach;
   }
   ###
   ### -----------------
