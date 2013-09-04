5bbe4301bd699b3ba9a744fce8e7bf04f20ebb4d
Henry Van Styn <vanstyn@intellitree.com>
2013-09-04 09:07:18 -0400

    Req Catalyst 5.90042 + moved pereqs from RapidApp.pm to dist.ini

--- lib/RapidApp.pm
+++ lib/RapidApp.pm
@@ -4,19 +4,9 @@ use warnings;
 
 our $VERSION = 0.99017;
 
+use Carp::Clan;
+
 # ABSTRACT: Turnkey ajaxy webapps
-
-# use to require some modules with specific min versions:
-use Catalyst 5.90002;
-use Plack 1.0029;
-use DBIx::Class 0.08250;
-use SQL::Translator 0.11016;
-use Template 2.25;
-use DateTime::Format::SQLite;
-use JavaScript::ExtJS::V3 '3.4.0';
-
-# quick Module version check cmd:
-#   perl -M"Catalyst 999" -e1
 
 use File::ShareDir qw(dist_dir);
 
@@ -24,7 +14,6 @@ sub share_dir {
   my $class = shift || __PACKAGE__;
   return $ENV{RAPIDAPP_SHARE_DIR} || dist_dir($class);
 }
-
 
 1;
 
