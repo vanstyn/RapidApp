vanstyn/RapidApp

commit 274cd8ef1691f47e12d53d44223b9d857ccf1414

LOCAL LINK CLICKS NOW TRANSFORMED INTO HASHPATHS
Clicking a *local* link such as <a href="/some/path"> now navigates to
the hashpath '#!/some/path' which will open in a tab.

Relative urls are also handled. For example, if the current hashpath is
'#!/tpl/wiki/foo.tt', clicking a link with href="blah.tt" would navigate
to '#!/tpl/wiki/blah.tt'

This was done so that template links can be consistent and work
correctly in both browser and Ext/tab context. Also, AppTab was recently
improved to be able to display raw text/html content in addition to
JSON-based Ext container configs, so this is now a reasonable default
behavior

share/assets/js/086-History.js
@@ -22,10 +22,12 @@ Ext.ns('Ext.ux.RapidApp');
 Ext.ux.RapidApp.HistoryInit = function() {
 	Ext.History.init();
 	Ext.History.on('change', Ext.ux.RapidApp.HashNav.handleHashChange);
+  Ext.ux.RapidApp.HashNav.INITIALIZED = true;
 }
 
 Ext.ux.RapidApp.HashNav = {
 	
+  INITIALIZED: false,
 	INIT_LOCATION_HASH: window.location.hash,
 	INIT_TITLE: document.title,
 	ignoreHashChange: false,
@@ -221,7 +223,30 @@ Ext.ux.RapidApp.HashNav = {
 		else {
 			document.title = title + ' - ' + Ext.ux.RapidApp.HashNav.INIT_TITLE;
 		}
-	}
+	},
+  
+  // New: util function - converts a normal URL into a hashpath
+  urlToHashPath: function(url) {
+    //absolute: 
+    var hashpath = '#!' + url;
+    
+    // relative:
+    if(url.search('/') !== 0) {
+      var parts = window.location.hash.split('/');
+      if(parts[0] == '#!') {
+        // If we're already at a hashnav path, use it as the base:
+        parts.pop();
+        parts.push(url);
+        hashpath = parts.join('/');
+      }
+      else {
+        // make absolute:
+        hashpath = '#!/' + url
+      }
+    }
+    
+    return hashpath;
+  }
 };
 
 

share/assets/js/500-RapidApp.js
@@ -5412,3 +5412,50 @@ Ext.ux.RapidApp.jsonArrArrToHtmlTable = function(v) {
 	return table_markup ? table_markup : v;
 }
 
+
+Ext.ux.RapidApp.Viewport = Ext.extend(Ext.Viewport, {
+
+  transform_links: false,
+
+  initComponent: function() {
+    if(this.transform_links && Ext.ux.RapidApp.HashNav.INITIALIZED) {
+      this.on('afterrender',function(){
+        this.getEl().on('click',this.clickInterceptor,this);
+      },this);
+    }
+    return Ext.ux.RapidApp.Viewport.superclass.initComponent.call(this);
+  },
+  
+  // TODO: why doesn't '^\w+://' work????? - Would like to match 'foo://'
+  externalUrlRe: new RegExp('^(http|https)://'),
+  
+  clickInterceptor: function(event) {
+    
+    var target = event.getTarget(null,null,true);
+    var match = ( target
+      // Is a link (<a> tag):
+      && target.is('a')
+      
+      // has no target attribute (i.e. target="_blank" or the like specified)
+      && ! target.getAttribute('target')
+    );
+    if(!match) { return; }
+    
+    var href = target.getAttribute('href');
+    match = ( href
+      // Is not already a hash url:
+      && href.search('#') !== 0
+      
+      // URL is local (does not start with http://, https://)
+      && ! this.externalUrlRe.test(href)
+    );
+    
+    if(match) {
+      // Stop the link click event and convert to hashpath:
+      event.stopEvent();
+      var hashpath = Ext.ux.RapidApp.HashNav.urlToHashPath(href);
+      window.location.hash = hashpath;
+    }
+  }
+
+});

share/templates/rapidapp/ext_viewport.tt
@@ -8,36 +8,37 @@
 
 <script type="text/javascript">
 
-	Ext.onReady(function(){
+  Ext.onReady(function(){
 
-        Ext.ux.RapidApp.HistoryInit();
+    Ext.ux.RapidApp.HistoryInit();
 
-		[% IF onReady_first %] [% onReady_first %] [% END %]
+    [% IF onReady_first %] [% onReady_first %] [% END %]
 
-		var config_url = '[% config_url %]';
-		var config_params = [% IF config_params %] [% config_params %] [% ELSE %] {} [% END %] ;
-		
-		new Ext.Viewport({
-			layout : 'fit',
-			hideBorders : true,
-			defaults : {
-				frame : true,
-				plit : true
-			},
-			items : {
-				xtype: 'dyncontainer',
-				layout: 'fit',
-				id: 'maincontainer',
-				itemsurl: config_url,
-				urlparams: config_params,
-				autoEl: {},
-				items: {
-					autoEl: {},
-					html: 'Loading, please wait...'
-				}
-			}
-		});
-	});
+    var config_url = '[% config_url %]';
+    var config_params = [% IF config_params %] [% config_params %] [% ELSE %] {} [% END %] ;
+
+    new Ext.ux.RapidApp.Viewport({
+      transform_links: true,
+      layout : 'fit',
+      hideBorders : true,
+      defaults : {
+        frame : true,
+        plit : true
+      },
+      items : {
+        xtype: 'dyncontainer',
+        layout: 'fit',
+        id: 'maincontainer',
+        itemsurl: config_url,
+        urlparams: config_params,
+        autoEl: {},
+        items: {
+          autoEl: {},
+          html: 'Loading, please wait...'
+        }
+      }
+    });
+  });
 
 </script>
 

commit 8733c3c3df7e8de927c14e4ed479ab1d19fe74bf

Fixed broken history/back button (regression from 274cd8ef169)

share/assets/js/086-History.js
@@ -22,12 +22,10 @@ Ext.ns('Ext.ux.RapidApp');
 Ext.ux.RapidApp.HistoryInit = function() {
 	Ext.History.init();
 	Ext.History.on('change', Ext.ux.RapidApp.HashNav.handleHashChange);
-  Ext.ux.RapidApp.HashNav.INITIALIZED = true;
 }
 
 Ext.ux.RapidApp.HashNav = {
 	
-  INITIALIZED: false,
 	INIT_LOCATION_HASH: window.location.hash,
 	INIT_TITLE: document.title,
 	ignoreHashChange: false,