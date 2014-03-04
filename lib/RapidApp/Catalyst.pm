3495554ea65dc4c6d196b37102d089e744c3a80a
Henry Van Styn <vanstyn@intellitree.com>
2014-03-04 17:49:16 -0500

    Deprecated RapidApp::Catalyst now throws die (placeholder)

--- lib/RapidApp/Catalyst.pm
+++ lib/RapidApp/Catalyst.pm
@@ -1,17 +1,6 @@
 package RapidApp::Catalyst;
 
-# Built-in plugins required for all RapidApp Applications:
-use Catalyst qw(
-+RapidApp::Role::CatalystApplication
-+RapidApp::CatalystX::SimpleCAS
-+RapidApp::Role::AssetControllers
-);
-
-use Moose;
-extends 'Catalyst';
-
-use RapidApp::AttributeHandlers;
-
+die "RapidApp::Catalyst is DEPRECATED - please use Catalyst::Plugin::RapidApp";
 
 
 1;
