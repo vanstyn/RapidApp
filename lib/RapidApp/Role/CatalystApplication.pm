4e35501c8712a8820b80d9e3bb4fb8533953684c
Henry Van Styn <vanstyn@intellitree.com>
2014-03-06 11:43:43 -0500

    disable_component_resolution_regex_fallback setting forced on
    The Catalyst config setting
    'disable_component_resolution_regex_fallback' is now forced to on/true.
    
    See in-code comments for more details

--- lib/RapidApp/Role/CatalystApplication.pm
+++ lib/RapidApp/Role/CatalystApplication.pm
@@ -21,6 +21,12 @@ has 'request_id' => ( is => 'ro', default => sub { (shift)->rapidApp->requestCou
 
 around 'setup_components' => sub {
 	my ($orig, $app, @args)= @_;
+  
+  # Force this standard setting. When it is off, in certain cases, it
+  # can lead to bizzare regex exceptions. This setting is already automatically
+  # set for all new apps created by recent versions of catalyst.pl
+  $app->config( disable_component_resolution_regex_fallback => 1 );
+  
   $app->$orig(@args);  # standard catalyst setup_components
   $app->setupRapidApp; # our additional components needed for RapidApp
 };
