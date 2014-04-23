6758ac6c6bfa2c7c85a3ebb4bbcec5f446e884c7
Henry Van Styn <vanstyn@intellitree.com>
2014-04-23 08:33:35 -0400

    Dynamic set local tmp dir for testapp

--- t/var/testapps/TestRA-ChinookDemo/lib/TestRA/ChinookDemo.pm
+++ t/var/testapps/TestRA-ChinookDemo/lib/TestRA/ChinookDemo.pm
@@ -3,6 +3,46 @@ package # hide from PAUSE
 use Moose;
 use namespace::autoclean;
 
+# ----------
+# Dynamically set a local TMPDIR to be relative to our
+# expected directory structure within the RapidApp t/
+# directory. We want to always end up with 't/var/tmp'
+BEGIN {
+  use Path::Class qw(dir);
+  use FindBin '$Bin';
+  
+  my $dir = dir($Bin);
+  while(-d $dir->parent) {
+    if (-d $dir->subdir('testapps')) {
+      # we're done
+      last;
+    }
+    elsif(-d $dir->subdir('var') && -d $dir->subdir('var')->subdir('testapps')) {
+      # This code path happens when we're called from a .t script
+      $dir = $dir->subdir('var');
+      last;
+    }
+    else {
+      # keep walking up parent directories (this code path happens when we're
+      # being called from a script within the test app)
+      $dir = $dir->parent;
+    }
+  }
+  
+  die "Unable to resolve test tmp dir" unless (-d $dir->subdir('testapps'));
+  
+  my $tmpdir = $dir->subdir('tmp')->absolute;
+  $tmpdir->mkpath unless (-d $tmpdir);
+  
+  # Make sure it actually exists/was created:
+  die "Error resolving/creating test tmp dir '$tmpdir'" unless (-d $tmpdir);
+  
+  # Now, set the TMPDIR env variable (used by Catalyst::Utils::class2tempdir)
+  $ENV{TMPDIR} = $tmpdir->stringify;
+}
+# ----------
+
+
 use Catalyst::Runtime 5.80;
 
 use RapidApp;
