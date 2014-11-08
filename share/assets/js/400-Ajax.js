vanstyn/RapidApp

commit 2fe7e94e995b7b1d7b83a6ec47af4d776017b4e2

AutoPanel reload() now calls purgeListeners()
See lengthy in-code comments

share/assets/js/400-Ajax.js
@@ -499,6 +499,27 @@ Ext.ux.AutoPanel = Ext.extend(Ext.Panel, {
   },
   
   reload: function() {
+    // Call removeAll now so that any listeners associated with remove/destroy can
+    // be called early (removeAll also gets called during the load process later).
+    // This is being done mainly to accomidate unsaved change detection in 
+    // DataStorePlus-driven components, but is the right/clean thing to do 
+    // regardless. Note that this is still not totally ideal, because it is already
+    // too late for the user to stop the reload (as with a simple close) but at
+    // least they have one last chance to save the outstanding changes which is
+    // better than nothing. 
+    // UPDATE -- don't do this after all because it can lead to a deadlock situation
+    //           just purge the listeners b/c it is too late for the user to save 
+    //           their changes if they clicked reload on the tab. Just like there
+    //           is nothing we can do if they refreshed the browser.
+    //this.removeAll();
+    // TODO: hook into the guts of this process to support
+    // actually cancelling the reload. This would need to be done by calling and
+    // testing the result of 'beforeremove'
+
+    // And then purge any listeners for a clean, fresh start. 
+    this.purgeListeners();
+
+    // Now call load using the same/original autoLoad config:
     this.load(this.autoLoad);
   },
   

share/assets/js/080-DataStore.js
@@ -454,6 +454,12 @@ Ext.ux.RapidApp.Plugin.CmpDataStorePlus = Ext.extend(Ext.util.Observable,{
 		
 		store.addEvents('beforeremove');
 		store.removeOrig = store.remove;
+    // This is a bastardized thing - remove() is documented as only to be called when removing
+    // a record from the store, but somehow it is also being called from the container removal
+    // machinery - this is either an ExtJS "bug" or a bug in ourselves someplace... In any case,
+    // when the latter happens 'record' below is actually the component being removed, and we
+    // hook into this and fire the beforeremove event which we are listening to for unsaved
+    // change detection (see our beforeRemoveConfirm() function further down)
 		store.remove = function(record) {
 			if(store.fireEvent('beforeremove',store,record) !== false) {
 				return store.removeOrig.apply(store,arguments);
@@ -1444,7 +1450,7 @@ Ext.ux.RapidApp.Plugin.CmpDataStorePlus = Ext.extend(Ext.util.Observable,{
 		}
 		
 		component.confirmRemoveInProg = true;
-		
+
 		var store = this.cmp.store;
 		if(!store || !store.data || !store.hasAnyPendingChanges()) { 
 			c.un('beforeremove',this.beforeRemoveConfirm,this);

commit b296d45d95c657e1f23742249211861e397b2691

AutoPanel reload() only purge 'beforeremove' (fix)
This fixes a regression introduced in 2fe7e94e995 where resizing of
child containers was broken after reloading the tab. See in-code
comments for details.

Also fix possible uninitialized warning in util func

share/assets/js/400-Ajax.js
@@ -515,9 +515,19 @@ Ext.ux.AutoPanel = Ext.extend(Ext.Panel, {
     // TODO: hook into the guts of this process to support
     // actually cancelling the reload. This would need to be done by calling and
     // testing the result of 'beforeremove'
-
-    // And then purge any listeners for a clean, fresh start. 
-    this.purgeListeners();
+    
+    // NEW/Updated:
+    // Clear *only* 'beforeremove' events -- we can't call purgeListeners()
+    // because it breaks things like resize events. We only really need to
+    // escape 'beforeremove' because, again, its too late at this point to abort
+    // the remove, which is what beforeremove is for (as discussed above)
+    var befRem = this.events.beforeremove;
+    if(befRem && typeof befRem == 'object') {
+      befRem.clearListeners();
+    }
+    
+    // Purge any child listeners for good measure (probably not needed)
+    this.items.each(function(itm) { itm.purgeListeners(); });
 
     // Now call load using the same/original autoLoad config:
     this.load(this.autoLoad);

lib/RapidApp/DBIC/Component/TableSpec.pm
@@ -1021,6 +1021,8 @@ sub proxy_method_get_changed {
 	my $self = shift;
 	my $method = shift;
 	
+  no warnings 'uninitialized'; # because we might compare undef values
+  
 	my $origRow = $self;
 	my %old = ();
 	if($self->in_storage) {