vanstyn/RapidApp

commit d445bfbd1ed81858c4b1eb78ff0c8e6f4c3f22eb

Fix caching bug in DbicLink2 rest_query/base_params
Changed the way 'rest_query' is applied to baseParams of the DataStore
to utilize the built-in DataStore2 handling rather than apply directly,
which can get cached in the object across multiple requests

(set dev version 0.99501_08)

lib/RapidApp.pm
@@ -5,7 +5,7 @@ use warnings;
 # Min supported Perl is currently v5.10
 use 5.010;
 
-our $VERSION = 0.99501_07;
+our $VERSION = 0.99501_08;
 
 # ABSTRACT: Turnkey ajaxy webapps
 

lib/RapidApp/Role/DbicLink2.pm
@@ -323,11 +323,18 @@ sub prepare_rest_request {
 	# (see the req_Row and and supplied_id methods in that class) while 'rest_query' is handled by
 	# all modules with the DbicLink2 role. Need to consolidate these in DbicLink2 so this all happens in 
 	# the same place
-	my $params = $key eq $self->record_pk ? { $key => $val } : { rest_query => $key . '/' . $val };
-	%{$self->c->req->params} = ( %{$self->c->req->params}, %$params );
-	my $baseParams = $self->DataStore->get_extconfig_param('baseParams') || {};
-	%$baseParams = ( %$baseParams, %$params );
-	$self->DataStore->apply_extconfig( baseParams => $baseParams );
+  my $params = $key eq $self->record_pk
+    ? { $key => $val }
+    : { rest_query => $key . '/' . $val };
+
+  %{$self->c->req->params} = ( %{$self->c->req->params}, %$params );
+
+  my $existBP = $self->c->req->params->{base_params} 
+    ? try{decode_json_utf8($self->c->req->params->{base_params})}
+    : {};
+
+  %$params = (%$existBP, %$params);
+  $self->c->req->params->{base_params} = encode_json_utf8($params);
 	# ---
 	
 }

commit 7acb4ea59900aac2ecb0b24bcb80acd697dd71a5

DbicLink2: fix create/update regression from d445bfbd1e
When creating and updating records, DbicLink2 calls DataStore->read in
order to validate the changes it just made, and send the new state of
records back to the client. The params of the active request are now
being cleared prior to doing this to prevent polluting the nested
request with DataStore-specific params present in the real request, such
as and specifically 'rest_query'. This was the real bug and the reason
the code changed in d445bfbd1e was so funky looking -- it was a
workaround for this, actual problem. The change in d445bfbd1e re-exposed
this problem, which is now being fixed properly in this commit

(set dev version 0.99501_10)

lib/RapidApp.pm
@@ -5,7 +5,7 @@ use warnings;
 # Min supported Perl is currently v5.10
 use 5.010;
 
-our $VERSION = 0.99501_09;
+our $VERSION = 0.99501_10;
 
 # ABSTRACT: Turnkey ajaxy webapps
 

lib/RapidApp/Role/DbicLink2.pm
@@ -2031,9 +2031,15 @@ sub _dbiclink_update_records {
 		#die usererr rawhtml $self->make_dbic_exception_friendly($err), title => 'Database Error';
 	};
 	
-	# Perform a fresh lookup of all the records we just updated and send them back to the client:
-	my $newdata = $self->DataStore->read({ columns => [ keys %{ $arr->[0] } ], id_in => \@updated_keyvals });
-	
+  # --
+  # Perform a fresh lookup of all the records we just updated and send them back to the client:
+  %{ $self->c->req->params } = (); #<-- must clear the request params to prevent polluting the read
+  my $newdata = $self->DataStore->read({
+    columns => [ keys %{ $arr->[0] } ], 
+    id_in   => \@updated_keyvals
+  });
+  # --
+  
 	## ----------------
 	# NEW: We need to make sure the order of the returned rows matches the supplied rows;
 	# Ext's data store uses the order rather than the record ids to match. If we don't do
@@ -2362,8 +2368,14 @@ sub _dbiclink_create_records {
 		#die usererr rawhtml $self->make_dbic_exception_friendly($err), title => 'Database Error';
 	};
 	
-	# Perform a fresh lookup of all the records we just updated and send them back to the client:
-	my $newdata = $self->DataStore->read({ columns => \@req_columns, id_in => \@updated_keyvals });
+  # --
+  # Perform a fresh lookup of all the records we just updated and send them back to the client:
+  %{ $self->c->req->params } = (); #<-- must clear the request params to prevent polluting the read
+  my $newdata = $self->DataStore->read({
+    columns => \@req_columns, 
+    id_in   => \@updated_keyvals
+  });
+  # --
 	
 	die usererr rawhtml "Unknown error; no records were created", 
 		title => 'Create Failed' unless ($newdata && $newdata->{results});