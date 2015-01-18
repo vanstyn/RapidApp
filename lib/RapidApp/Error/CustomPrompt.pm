3f68d7aa09517ad97a3e9bb00e6fd7bd9425a9c1
Henry Van Styn <vanstyn@intellitree.com>
2015-01-18 10:59:56 -0500

    Remove deprecated RapidApp::Error* pkgs - #106

--- lib/RapidApp/Error/CustomPrompt.pm
+++
@@ -1,8 +0,0 @@
-package RapidApp::Error::CustomPrompt;
-
-use strict;
-use warnings;
-
-die "Use RapidApp::Responder::CustomPrompt instead of RapidApp::Error::CustomPrompt";
-
-1;
--- lib/RapidApp/Error/UserError.pm
+++
@@ -1,8 +0,0 @@
-package RapidApp::Error::UserError;
-
-use strict;
-use warnings;
-
-die "Use RapidApp::Responder::UserError instead of RapidApp::Error::UserError";
-
-1;
