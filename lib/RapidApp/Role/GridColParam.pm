6f33727873b615bd8c007c3fe4d23fc2d16a4ab7
Henry Van Styn <vanstyn@intellitree.com>
2015-01-19 14:44:30 -0500

    remove deprecated RapidApp::Role::GridColParam - #106
    (set dev ver 0.99699_08)

--- lib/RapidApp/Role/GridColParam.pm
+++
@@ -1,8 +0,0 @@
-package RapidApp::Role::GridColParam;
-
-use Moose::Role;
-
-# Trait for RapidApp:Column attributes to indentify the attribute as a "Grid" attribute,
-# for modules like AppGrid2
-
-1;
