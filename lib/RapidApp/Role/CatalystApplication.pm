d06c7bca16dfa143c8299849ebf3c87c73d100e1
Henry Van Styn <vanstyn@intellitree.com>
2015-01-19 20:47:26 -0500

    Remove deprecated view RapidApp::View::OnError - #106

--- lib/RapidApp/Role/CatalystApplication.pm
+++ lib/RapidApp/Role/CatalystApplication.pm
@@ -110,7 +110,6 @@ sub setupRapidApp {
     ['RapidApp::View::Printview'     => 'View::RapidApp::Printview'  ],
     ['RapidApp::View::JSON'          => 'View::RapidApp::JSON'       ],
     ['RapidApp::View::HttpStatus'    => 'View::RapidApp::HttpStatus' ],
-    ['RapidApp::View::OnError'       => 'View::RapidApp::OnError'    ],
     ['RapidApp::View::Template'      => 'View::RapidApp::Template'   ]
   );
   
