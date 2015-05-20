60b7e5afdc1f26acf22a9a16832d899cf714a505
Henry Van Styn <vanstyn@intellitree.com>
2015-05-20 10:33:43 -0400

    Require Catalyst::Controller::SimpleCAS 0.996 (runtime)

--- lib/RapidApp/Role/CatalystApplication.pm
+++ lib/RapidApp/Role/CatalystApplication.pm
@@ -18,7 +18,7 @@ use URI::Escape;
 use RapidApp;
 use Template;
 
-use Catalyst::Controller::SimpleCAS 0.995;
+use Catalyst::Controller::SimpleCAS 0.996;
 
 sub rapidapp_version { $RapidApp::VERSION }
 
