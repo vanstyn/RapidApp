84583b33eff9cc8b35ff20065c958c39c2822c82
Henry Van Styn <vanstyn@intellitree.com>
2015-06-03 08:44:54 -0400

    Require latest Catalyst (build) and SimpleCAS (runtime)
    (set dev ver 1.0401_15)

--- lib/RapidApp/Role/CatalystApplication.pm
+++ lib/RapidApp/Role/CatalystApplication.pm
@@ -18,7 +18,7 @@ use URI::Escape;
 use RapidApp;
 use Template;
 
-use Catalyst::Controller::SimpleCAS 0.997;
+use Catalyst::Controller::SimpleCAS 0.998;
 
 sub rapidapp_version { $RapidApp::VERSION }
 
