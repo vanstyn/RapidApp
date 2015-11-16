vanstyn/RapidApp

commit 83c15d44f0ebf2c2c5ebbf1c4b1d33e2cab34f36

Expanded logic for 'ra-dsapi-deny-update' + tie-in store-button css
The flag to record that the crud/api action 'update' is now also set
in the case of no editable columns (and is also now recalculated on
store metachange instead of only at render).

Updated the CSS for AppDV store buttons to also disable when the
corresponding API is not available (this is somewhat overlap, however
it handles cases such as the delete button which never gets the
enable/disable events when it is excluded altogether, since for the
grid, which was the orginal store-button implementation, for these
cases the button is simply not setup at all)

Also fixed some JS undef bugs in AppDV

(set dev ver 1.0601_17)

dist.ini
@@ -4,7 +4,7 @@ license = Perl_5
 copyright_holder = IntelliTree Solutions llc
 copyright_year   = 2013
 
-version = 1.0601_16
+version = 1.0601_17
 
 [MetaResources]
 homepage        = http://www.rapidapp.info

lib/RapidApp.pm
@@ -5,7 +5,7 @@ use warnings;
 # Min supported Perl is currently v5.10
 use 5.010;
 
-our $VERSION = 1.0601_16;
+our $VERSION = 1.0601_17;
 
 # ABSTRACT: Turnkey ajaxy webapps
 

share/assets/js/080-DataStore.js
@@ -586,6 +586,9 @@ Ext.ux.RapidApp.Plugin.CmpDataStorePlus = Ext.extend(Ext.util.Observable,{
 				// request to request, so we can disable the edit button when that number is 0
 				store.editable_fields_count = edit_count;
 			}
+      
+      this.updateCssForStoreAPI();
+      
 		},this);
 		
 		store.hasLoadedColumn = function(name) {
@@ -1601,9 +1604,16 @@ Ext.ux.RapidApp.Plugin.CmpDataStorePlus = Ext.extend(Ext.util.Observable,{
   updateCssForStoreAPI: function() {
     var El = this.cmp.getEl(), store = this.cmp.store;
     var apis = ['create','read','update','destroy'];
+    var testFn = function(api) {
+      if(!store.api[api]) { return false; }
+      // Extra test for update -- 
+      // if there are no editable columns, consider update as a whole denied:
+      if(api == 'update' && store.editableFieldsCount() == 0) { return false; }
+      return true;
+    };
     Ext.each(apis,function(api){
       var cls = ['ra-dsapi-deny-',api].join('');
-      if(store.api[api]) {
+      if(testFn(api)) {
         if(El.hasClass(cls)) { El.removeClass(cls); }
       }
       else {
@@ -1611,7 +1621,6 @@ Ext.ux.RapidApp.Plugin.CmpDataStorePlus = Ext.extend(Ext.util.Observable,{
       }
     },this);
   },
-  
 	
 	beforeRemoveConfirm: function(c,component) {
 		if(component != this.cmp) {

share/assets/css/045-AppDV.css
@@ -524,12 +524,15 @@ div.appdv-tt-generated .editable-value .appdv-edit-field .data-holder {
   cursor: pointer;
 }
 
-.ra-dsbtn-deny-add    .appdv-tt-generated .store-button.add,
-.ra-dsbtn-deny-edit   .appdv-tt-generated .store-button.edit,
-.ra-dsbtn-deny-delete .appdv-tt-generated .store-button.delete,
-.ra-dsbtn-deny-reload .appdv-tt-generated .store-button.reload,
-.ra-dsbtn-deny-save   .appdv-tt-generated .store-button.save,
-.ra-dsbtn-deny-undo   .appdv-tt-generated .store-button.undo {
+.ra-dsbtn-deny-add     .appdv-tt-generated .store-button.add,
+.ra-dsapi-deny-create  .appdv-tt-generated .store-button.add,
+.ra-dsbtn-deny-edit    .appdv-tt-generated .store-button.edit,
+.ra-dsapi-deny-update  .appdv-tt-generated .store-button.edit,
+.ra-dsbtn-deny-delete  .appdv-tt-generated .store-button.delete,
+.ra-dsapi-deny-destroy .appdv-tt-generated .store-button.delete,
+.ra-dsbtn-deny-reload  .appdv-tt-generated .store-button.reload,
+.ra-dsbtn-deny-save    .appdv-tt-generated .store-button.save,
+.ra-dsbtn-deny-undo    .appdv-tt-generated .store-button.undo {
   opacity: 0.2;
   cursor: default;
 }

share/assets/js/510-AppDV.js
@@ -115,6 +115,9 @@ Ext.ux.RapidApp.AppDV.DataView = Ext.extend(Ext.DataView, {
       },this);
     }
     
+    // init
+    this.FieldCmp = {};
+    
 	},
 	
 	onBeforeselect: function() {
@@ -660,9 +663,9 @@ Ext.ux.RapidApp.AppDV.DataView = Ext.extend(Ext.DataView, {
 		var dataEl = editEl.child('div.data-holder');
 		var fieldEl = editEl.child('div.field-holder');
 		
-    var Fld = this.FieldCmp[index][fieldname];
-    if(Fld && dataWrap && dataEl && fieldEl) {
-			
+    
+    if(this.FieldCmp[index] && this.FieldCmp[index][fieldname] && dataWrap && dataEl && fieldEl) {
+			var Fld = this.FieldCmp[index][fieldname];
 			if(Fld.contentEl) {
 				Fld.contentEl.appendTo(dataWrap);
 			}

commit 98a1f59d8e21ab83618ca4836e9524b498c5da1e

updateCssForStoreAPI() now only runs if rendered (bugfix)
Fix regression introduced in 83c15d44f0ebf2c2c5ebbf1c4b1d33e2cab34f36
which calls the DataStore updateCssForStoreAPI() function on every store
metachange to NOT call the logic unless the component is rendered. There
are cases (likely race conditions) where the metachange event is called
before the render has completed, and things break when this happens.
Modified the logic in the function to return unless the component
rendered flag is true.

This was a bug discovered in a legacy application which involves a
slower page consisting of multiple and large datastore components
(AppDVs and grids) which likley triggered a race-condition

(set dev ver 1.1000_02)

dist.ini
@@ -4,7 +4,7 @@ license = Perl_5
 copyright_holder = IntelliTree Solutions llc
 copyright_year   = 2013
 
-version = 1.1000
+version = 1.1000_02
 
 [MetaResources]
 homepage        = http://www.rapidapp.info

lib/RapidApp.pm
@@ -5,7 +5,7 @@ use warnings;
 # Min supported Perl is currently v5.10
 use 5.010;
 
-our $VERSION = 1.1000_01;
+our $VERSION = 1.1000_02;
 
 # ABSTRACT: Turnkey ajaxy webapps
 

share/assets/js/080-DataStore.js
@@ -1613,6 +1613,9 @@ Ext.ux.RapidApp.Plugin.CmpDataStorePlus = Ext.extend(Ext.util.Observable,{
   
   // Sets/clears a CSS class/flag for each CRUD action missing from the store api
   updateCssForStoreAPI: function() {
+    // Things will break if we try to run this logic before we're rendered:
+    if(!this.cmp.rendered) { return; }
+    
     var El = this.cmp.getEl(), store = this.cmp.store;
     var apis = ['create','read','update','destroy'];
     var testFn = function(api) {