50b7e4b5fcaa7b257526ff11138841fe8dce3127
Henry Van Styn <vanstyn@intellitree.com>
2016-04-01 12:04:26 -0400

    sourceable util automates setting shell vars for dev - fixes #166
    New util script can be called like:
    
     source devel/source_dev_shell_vars.bash
    
    from within a checkout of the repository to setup env vars to make that
    repo active in the current shell for development purposes.
    
    See in-code comments for more info

---
+++ devel/print_dev_exports.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/env perl
+
+# -----------------------------------------------------------------------------
+# This script dumps bash 'export' commands needed to update shell environment
+# variables. It is the internal guts for source_dev_shell_vars.bash and is
+# not intended to be called directly. The only reason this script exists is
+# because I was too lazy to write it in bash, even though the only way that
+# shell env vars can be updated is via 'sourcing' a bash script, which is what
+# the parent script source_dev_shell_vars.bash is for.
+#
+# #166 (https://github.com/vanstyn/RapidApp/issues/166)
+# -----------------------------------------------------------------------------
+
+use strict;
+use warnings;
+use List::Util;
+
+use Path::Class qw/file dir/;
+my $repo_dir = file($0)->parent->parent->absolute; 
+
+die "Doesn't look like I'm within a RapidApp repo" unless (
+  -f $repo_dir->subdir('script')->file('rdbic.pl')
+);
+
+my $exports = {
+  PERLLIB => &_prepend_colon_list( $ENV{PERLLIB}, $repo_dir->subdir('lib')->stringify ),
+  PATH    => &_prepend_colon_list( $ENV{PATH},    $repo_dir->subdir('script')->stringify ),
+  RAPIDAPP_SHARE_DIR => $repo_dir->subdir('share')
+};
+
+# Just print the export commands on STDOUT and exit:
+print join("\n", map { "export $_=$exports->{$_}" } keys %$exports);
+
+exit 0;
+
+
+##############################################
+
+sub _prepend_colon_list {
+  my ($clist, $add) = @_;
+  
+  my @list = split(/:/,($clist||''));
+  
+  return ( List::Util::first { $_ eq $add } @list )
+    ? $clist
+    : join(':',$add,@list)
+}
+
---
+++ devel/source_dev_shell_vars.bash
@@ -0,0 +1,48 @@
+# ------------------------------------------------------------------
+# 'source' this script to update ENV vars to make the RapidApp repo
+# this file is contained in active for your shell for development.
+#
+# This will update the following environment variables, preserving
+# existing values (i.e. appending vs replacing) appropriately
+#
+#  * PERLLIB
+#  * PATH
+#  * RAPIDAPP_SHARE_DIR
+#
+# #166 (https://github.com/vanstyn/RapidApp/issues/166)
+# ------------------------------------------------------------------
+
+if [ $0 == ${BASH_SOURCE[0]} ]; then
+  echo "Oops! This script needs to be 'sourced' to work -- like this:";
+  echo -e "\n  source $0\n";
+  exit 1;
+fi;
+
+do_source_vars() {
+  local dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+  local prl="print_dev_exports.pl"
+  
+  if [ -e "$dir/$prl" ]; then
+    local export_cmds;
+    export_cmds=`perl $dir/$prl`;
+    local exit=$?;
+    if [ $exit -ne 0 ]; then
+      echo "An error occured calling $dir/$prl (exit: $exit)";
+    else
+      eval $export_cmds;
+      exit=$?;
+      if [ $exit -ne 0 ]; then
+        echo "An unknown error occured attempting to export shell variables";
+      else
+        echo " -- Exported RapidApp env variables to your shell: --";
+        echo -e "\n$export_cmds\n";
+      fi;
+    fi;
+  else
+    echo "Error! $PRL script not found! Did you move this script?";
+    # note: we're not calling exit here because we've been sourced 
+    # and this would cause the parent shell to exit
+  fi;
+}
+
+do_source_vars;
