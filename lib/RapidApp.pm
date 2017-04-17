vanstyn/RapidApp

commit 10f05b76a25e5ff32686115a86d565bdc66861cd

PROPERLY FIX BUG PREVENTING COL PROFILES FROM SETTING allow_add/edit
The commit
https://github.com/vanstyn/RapidApp/commit/2dccadc6f307e55c2f0cb9d1974bd0c68cbdc033
from May 30 was a regression which fixed the long-standing problem of
column profiles not working correctly when they set allow_add/allow_edit
to OFF, but created a new bug which incorrectly made columns not addable
when editable is off. This is the case of these settings being set via
the creatable_colspec and updatable_colspec in the local module, which
is an order of operations problem -- the normalization of allow_add/edit
doesn't happen until AFTER the module code modifies these settings
according to its local colspec settings.

The fix, which is ugly and temporary, is that the column profiles are
now considered in the module code itself. This is the best solution,
before a long-planned refactor of this stuff can be done.

See also in-code comments.

(set dev ver 1.2000_04)

dist.ini
@@ -4,7 +4,7 @@ license = Perl_5
 copyright_holder = IntelliTree Solutions llc
 copyright_year   = 2013
 
-version = 1.2000_03
+version = 1.2000_04
 
 [MetaResources]
 homepage        = http://www.rapidapp.info

lib/RapidApp.pm
@@ -5,7 +5,7 @@ use warnings;
 # Min supported Perl is currently v5.10
 use 5.010;
 
-our $VERSION = 1.2000_03;
+our $VERSION = 1.2000_04;
 
 # ABSTRACT: Turnkey ajaxy webapps
 

lib/RapidApp/TableSpec/Column/Profile.pm
@@ -288,6 +288,24 @@ sub DEFAULT_PROFILES {{
 
 }};
 
+our %NO_ALLOW_ADD_PROFILES = ();
+our %NO_ALLOW_EDIT_PROFILES = ();
+{
+  my $Defs = &DEFAULT_PROFILES;
+  for my $prof (keys %$Defs) {
+    my $p = $Defs->{$prof};
+    my ($a,$e) = ($p->{allow_add},$p->{allow_edit});
+    if(exists $p->{allow_add}) {
+      $a = $$a if (ref($a));
+      $NO_ALLOW_ADD_PROFILES{$prof} = 1 if ("$a" eq '0'); 
+    }
+    if(exists $p->{allow_edit}) {
+      $e = $$e if (ref($e));
+      $NO_ALLOW_EDIT_PROFILES{$prof} = 1 if ("$e" eq '0'); 
+    }
+  }
+}
+
 
 our $SKIP_BASE = 0;
 

lib/RapidApp/TableSpec/Role/DBIC.pm
@@ -271,27 +271,43 @@ sub add_db_column($@) {
 	}
 	# --
   
+  ## -- (see also below comment and commit 2dccadc6f3 which was a regression)
+  ## Hate having to do this, but we have to consider the column profiles here,
+  ## because this happens before they are applied (->add_columns, at the end of this
+  ## method). Specifically, we need to consider profiles which turn OFF allow_add
+  ## and allow_edit, otherwise, we'll clobber those settings if we want to turn
+  ## them on here. And we do need to turn them on, specifically again for the case of
+  ## creatable but not editable, because the default of editable changes the default
+  ## for creatable.
+  $creatable = 0 if List::Util::first {
+    $RapidApp::TableSpec::Column::Profile::NO_ALLOW_ADD_PROFILES{$_}
+  } @{$opt{profiles}||[]};
+  
+  $editable = 0 if List::Util::first {
+    $RapidApp::TableSpec::Column::Profile::NO_ALLOW_EDIT_PROFILES{$_}
+  } @{$opt{profiles}||[]};
+  ## --
+  
+  
 	$opt{allow_edit} = \0 unless ($editable);
 	$opt{allow_add} = \0 unless ($creatable);
   
-  # UPDATE: no longer flip these flags on since this logic should now be reduntant
-  # to the allow_add/edit normalization in RapidApp::Module::DatStor::Column and
-  # this was also conflicting with column profiles which attempt to set these flags
-  #### New: flip the allow edit/add flags on if they are not already set to something,
-  #### and no_column is not set. This is needed for the case of creatable but not
-  #### editable, since the default allow_add is based on the value of allow_edit, which
-  #### is intended for the case of it being set by the user
-  ###unless(jstrue($opt{no_column})) {
-  ###  $opt{allow_edit} //= \1 if ($editable);
-  ###  $opt{allow_add}  //= \1 if ($creatable);
-  ###}
+  #### We do need this code after all -- see above comment. Was removed in 2dccadc6f3
+  # New: flip the allow edit/add flags on if they are not already set to something,
+  # and no_column is not set. This is needed for the case of creatable but not
+  # editable, since the default allow_add is based on the value of allow_edit, which
+  # is intended for the case of it being set by the user
+  unless(jstrue($opt{no_column})) {
+    $opt{allow_edit} //= \1 if ($editable);
+    $opt{allow_add}  //= \1 if ($creatable);
+  }
 
 	unless ($editable or $creatable) {
 		$opt{rel_combo_field_cnf} = $opt{editor} if($opt{editor});
 		$opt{editor} = '' ;
 	}
-	
-	return $self->add_columns(\%opt);
+
+	$self->add_columns(\%opt);
 }
 
 

commit a79938ad09622c253ba28fbac740cdb8d7af24dd

'hidden' col profile explicitly sets allow_add/edit off
This is the bug that just won't die...

A regression caused by 10f05b76a25e5ff32686115a86d565bdc66861cd made it
so that the 'hidden' column profile no longer implied allow_add and
allow_edit to be false. However, this is actually expected based on the
change in that commit. It seems as though it was an oversight having not
explicitly set those options at the time. Hopefully its really finally
fixed now

(set dev ver 1.2111_01)

dist.ini
@@ -4,7 +4,7 @@ license = Perl_5
 copyright_holder = IntelliTree Solutions llc
 copyright_year   = 2013
 
-version = 1.2111
+version = 1.2111_01
 
 [MetaResources]
 homepage        = http://www.rapidapp.info

lib/RapidApp.pm
@@ -5,7 +5,7 @@ use warnings;
 # Min supported Perl is currently v5.10
 use 5.010;
 
-our $VERSION = '1.2111';
+our $VERSION = 1.2111_01;
 
 # ABSTRACT: Turnkey ajaxy webapps
 

lib/RapidApp/TableSpec/Column/Profile.pm
@@ -278,7 +278,12 @@ sub DEFAULT_PROFILES {{
     auto_editor_params => { user_editable => 1 }
   },
   hidden => {
-    no_column => \1, no_quick_search => \1, no_multifilter => \1
+    no_column => \1, 
+    allow_add => \0, 
+    allow_edit => \0,
+    allow_batchedit => \0,
+    no_quick_search => \1, 
+    no_multifilter => \1
   },
   multirel => {
     multifilter_type => 'number',