b25bbfb70d6dbf540fa7a168c898f9bd6e64cc7e
Henry Van Styn <vanstyn@intellitree.com>
2017-05-14 10:34:00 -0400

    MdEditor: moved upload logic using slight-of-hand (WIP)
    Using a trick, moved the function which handles the upload/insert into
    the parent ExtJS field object, even though its running in a different
    context. Seems to be working fine, but the verdict is still out to see
    if other issues arise...
    
    IN PROGRESS...

--- share/assets/js/083-MdEditor.js
+++ share/assets/js/083-MdEditor.js
@@ -21,6 +21,10 @@ iframeHtml:
   ></textarea>
   <script>
     window.document.simplemde = new SimpleMDE({
+      picoModal: picoModal,
+      customUploadActionFn: function(){ 
+        throw " !! virtual method customUploadActionFn was not set to a real function!"; 
+      },
       element: document.getElementById("ta-target"),
       forceSync: true,
       spellChecker: false,
@@ -33,87 +37,7 @@ iframeHtml:
         "|", {
           name: "custom",
           action: function customFunction(editor){
-          
-            // this is a proof-of-concept for a custom toolbar action that inserts text
-          
-            var modal, insertStr;
-            var cm = editor.codemirror;
-            var pos = cm.getCursor("start");
-            
-            
-            function doUpload(File, callback) {
-              callback = callback || function(){};
-              
-              var Xhr = new XMLHttpRequest();
-              Xhr.addEventListener('load',  function(E) { callback(E,'load')  }, false);
-              Xhr.addEventListener('error', function(E) { callback(E,'error') }, false);
-              Xhr.addEventListener('abort', function(E) { callback(E,'abort') }, false);
-              
-              var formData = new FormData();
-              formData.append('Filedata', File);
-              
-              Xhr.open('POST', '_ra-rel-mnt_/simplecas/upload_file');
-              Xhr.send(formData);
-            }
-            
-            var onClose = function(){
-            
-              if(insertStr) {
-                cm.replaceRange(insertStr,pos);
-                pos.ch = pos.ch + insertStr.length;
-              }
-              
-              cm.focus();
-              cm.doc.setCursor(pos);
-            }
-            
-            function onInputChange(e) {
-              var file = e.target.files[0];
-              
-              doUpload(file,function(E,event) {
-                var res = window.JSON.parse(E.currentTarget.responseText);
-                
-                console.dir(res);
-                
-                if(res.filename && res.checksum) {
-                  insertStr = [
-                    '[',res.filename,']',
-                    '(','_ra-rel-mnt_/simplecas/fetch_content/',res.checksum,'/',res.filename,')'
-                  ].join('');
-                  
-                  modal.close();
-                }
-                
-                
-              });
-              
-              //console.log('on input change!');
-            }
-            
-            var formHtml = [
-              '<form enctype="multipart/form-data" method="post" action="_ra-rel-mnt_/simplecas/upload_file">',
-              '<div><input type="file" name="Filedata" /></div>',
-              
-              '</form>'
-            
-            ].join('');
-            
-            
-            modal = picoModal({
-                content: "<h3>Input file</h3>" + formHtml,
-                modalStyles: function (styles) { styles.top = '60px'; },
-                focus: false,
-                width: 550
-              })
-              .afterCreate(function(m){
-                var input = m.modalElem().getElementsByTagName('input')[0];
-                if(input) {
-                  input.onchange = onInputChange;
-                }
-              })
-              .afterClose(onClose)
-              .show();
-
+            return editor.options.customUploadActionFn(editor);
           },
           className: "fa fa-star",
           title: "Custom Test",
@@ -166,6 +90,11 @@ iframeHtml:
       if(!this.iframeDom) { return null; }
       var simplemde = this.iframeDom.contentWindow.document.simplemde;
       if(!simplemde) { return null; }
+      
+      var scope = this;
+      simplemde.options.customUploadActionFn = function() {
+        return scope.customUploadActionFn.apply(scope,arguments);
+      }
       
       // this is needed to get the starting size to cooporate (FIXME)
       simplemde.codemirror.setSize(null,'100%');
@@ -255,6 +184,91 @@ iframeHtml:
   
   getValue : function(){
     return this.getRawValue();
+  },
+  
+  customUploadActionFn: function(editor) {
+  
+  
+    // this is a proof-of-concept for a custom toolbar action that inserts text
+  
+    var modal, insertStr;
+    var cm = editor.codemirror;
+    var pos = cm.getCursor("start");
+    
+    
+    function doUpload(File, callback) {
+      callback = callback || function(){};
+      
+      var Xhr = new XMLHttpRequest();
+      Xhr.addEventListener('load',  function(E) { callback(E,'load')  }, false);
+      Xhr.addEventListener('error', function(E) { callback(E,'error') }, false);
+      Xhr.addEventListener('abort', function(E) { callback(E,'abort') }, false);
+      
+      var formData = new FormData();
+      formData.append('Filedata', File);
+      
+      Xhr.open('POST', '_ra-rel-mnt_/simplecas/upload_file');
+      Xhr.send(formData);
+    }
+    
+    var onClose = function(){
+    
+      if(insertStr) {
+        cm.replaceRange(insertStr,pos);
+        pos.ch = pos.ch + insertStr.length;
+      }
+      
+      cm.focus();
+      cm.doc.setCursor(pos);
+    }
+    
+    function onInputChange(e) {
+      var file = e.target.files[0];
+      
+      doUpload(file,function(E,event) {
+        var res = window.JSON.parse(E.currentTarget.responseText);
+        
+        console.dir(res);
+        
+        if(res.filename && res.checksum) {
+          insertStr = [
+            '[',res.filename,']',
+            '(','_ra-rel-mnt_/simplecas/fetch_content/',res.checksum,'/',res.filename,')'
+          ].join('');
+          
+          modal.close();
+        }
+        
+        
+      });
+      
+      //console.log('on input change!');
+    }
+    
+    var formHtml = [
+      '<form enctype="multipart/form-data" method="post" action="_ra-rel-mnt_/simplecas/upload_file">',
+      '<div><input type="file" name="Filedata" /></div>',
+      
+      '</form>'
+    
+    ].join('');
+    
+    
+    modal = editor.options.picoModal({
+        content: "<h3>Input file</h3>" + formHtml,
+        modalStyles: function (styles) { styles.top = '60px'; },
+        focus: false,
+        width: 550
+      })
+      .afterCreate(function(m){
+        var input = m.modalElem().getElementsByTagName('input')[0];
+        if(input) {
+          input.onchange = onInputChange;
+        }
+      })
+      .afterClose(onClose)
+      .show();
+
   }
   
 });
