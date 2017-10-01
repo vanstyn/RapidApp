0660c5726f7bb91b54c606f5078f4fbd578678ff
Henry Van Styn <vanstyn@intellitree.com>
2017-10-01 11:36:20 -0400

    FORCE LEFT JOIN OVERRIDE MOVED TO OUTER SUB
    In cab4a6732887df79d060a1204631a8df1cecd673 experimental code was added
    to force all joins to left joins in DbicLnk (i.e. grid) modules. See
    that commit for the rationales for this change, and see the comment
    thread in GitHub for additional discussion.
    
    This code has been working, however, the location it was originally
    placed in ->rs_all only effected the rows, and not the associated count
    and summary function calls, which could lead these to display the
    incorrect result, excluding certain rows.
    
    Moved the override to the outer sub, ->read_records, so it is in effect
    for all three potential queries (rows, total count and summaries) for
    each page load
    
    (set dev ver 1.3003_01)

--- lib/RapidApp/Module/StorCmp/Role/DbicLnk.pm
+++ lib/RapidApp/Module/StorCmp/Role/DbicLnk.pm
@@ -701,6 +701,22 @@ sub read_records {
   my $self = shift;
   my $params = shift || $self->c->req->params;
   
+  ## ---------
+  ## Experimental override to force all joins to be LEFT joins, since in the
+  ## context of the grid, it is never helpful to inner join which can cause
+  ## rows to not show up when the foreign key isn't found, which is never what
+  ## we want to happen - TODO: add test cases for this
+  no warnings 'redefine';
+  my $orig_resolve_join = \&DBIx::Class::ResultSource::_resolve_join;
+  local *DBIx::Class::ResultSource::_resolve_join = sub {
+    my ($self, $join, $alias, $seen, $jpath, $parent_force_left) = @_;
+    return $orig_resolve_join->($self, $join, $alias, $seen, $jpath, 1)
+  };
+  # for more info, see the thread/convo on github: 
+  #  https://github.com/vanstyn/RapidApp/commit/cab4a6732
+  ## ---------
+  
+  
   my $Rs = $self->get_read_records_Rs($params);
   
   # -- Github Issue #10 - SQLite-specific fix --
@@ -785,21 +801,6 @@ sub apply_first_records {
 sub rs_all { 
   my ($self, $Rs) = @_;
   my $want = wantarray;
-  
-  ## ---------
-  ## Experimental override to force all joins to be LEFT joins, since in the
-  ## context of the grid, it is never helpful to inner join which can cause
-  ## rows to not show up when the foreign key isn't found, which is never what
-  ## we want to happen - TODO: add test cases for this
-  no warnings 'redefine';
-  my $orig_resolve_join = \&DBIx::Class::ResultSource::_resolve_join;
-  local *DBIx::Class::ResultSource::_resolve_join = sub {
-    my ($self, $join, $alias, $seen, $jpath, $parent_force_left) = @_;
-    return $orig_resolve_join->($self, $join, $alias, $seen, $jpath, 1)
-  };
-  # for more info, see the thread/convo on github: 
-  #  https://github.com/vanstyn/RapidApp/commit/cab4a6732
-  ## ---------
 
   # ----- GitHub Issue #165
   # NEW: extract the nested select refs from the special ''/-as structure for
